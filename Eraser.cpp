c522417de54a71018f7aa81ed1916cbb76ad3665
lowjoel <lowjoel@5a19fcc5-093c-0410-9d93-cd3e460611b7>
2007-10-09 10:19:36 +0000

    -Enable Edit and Continue in all our projects -Generate the library files in the bin directory, no need for a lib directory

--- Eraser.cpp
+++ Eraser.cpp
@@ -180,13 +180,6 @@ BOOL CEraserApp::InitInstance()
     // If you are not using these features and wish to reduce the size
     //  of your final executable, you should remove from the following
     //  the specific initialization routines you do not need.
-
-#ifdef _AFXDLL
-    Enable3dControls();         // Call this when using MFC in a shared DLL
-#else
-    Enable3dControlsStatic();   // Call this when linking to MFC statically
-#endif
-
     // Change the registry key under which our settings are stored.
 	if (!no_registry)
 	    SetRegistryKey(_T("Heidi Computers Ltd\\Eraser\\5.5"));
