96a8ebd961847360f7c78a38400c629a2ccd4a38
lowjoel <lowjoel@5a19fcc5-093c-0410-9d93-cd3e460611b7>
2007-10-10 09:36:05 +0000

    The PreferencesPage.cpp file should have the minimize to try disabled if the tray icon is disabled

--- trunk/PreferencesPage.cpp
+++ trunk/PreferencesPage.cpp
@@ -1,4 +1,5 @@
 // PreferencesPage.cpp
+// $Id$
 //
 // Eraser. Secure data removal. For Windows.
 // Copyright © 1997-2001  Sami Tolvanen (sami@tolvanen.com).
@@ -46,12 +47,12 @@ CPropertyPage(CEraserPreferencesPage::IDD)
 , m_bResolveLock(TRUE)
 , m_bResolveAskUser(TRUE)
 {
-    //{{AFX_DATA_INIT(CEraserPreferencesPage)
-    m_bClearSwap = FALSE;
-    m_bShellextResults = FALSE;
-    m_bResultsForFiles = FALSE;
-    m_bResultsForUnusedSpace = FALSE;
-    m_bResultsOnlyWhenFailed = FALSE;
+	//{{AFX_DATA_INIT(CEraserPreferencesPage)
+	m_bClearSwap = FALSE;
+	m_bShellextResults = FALSE;
+	m_bResultsForFiles = FALSE;
+	m_bResultsForUnusedSpace = FALSE;
+	m_bResultsOnlyWhenFailed = FALSE;
 	m_bErasextEnabled = FALSE;
 	m_bEnableSlowPoll = FALSE;
 	//}}AFX_DATA_INIT
@@ -84,10 +85,10 @@ void CEraserPreferencesPage::DoDataExchange(CDataExchange* pDX)
 
 
 BEGIN_MESSAGE_MAP(CEraserPreferencesPage, CPropertyPage)
-    //{{AFX_MSG_MAP(CEraserPreferencesPage)
-    ON_BN_CLICKED(IDC_CHECK_RESULTS_FOR_UNUSED_SPACE, OnCheckResultsForUnusedSpace)
-    ON_BN_CLICKED(IDC_CHECK_RESULTS_FOR_FILES, OnCheckResultsForFiles)
-    //}}AFX_MSG_MAP
+	//{{AFX_MSG_MAP(CEraserPreferencesPage)
+	ON_BN_CLICKED(IDC_CHECK_RESULTS_FOR_UNUSED_SPACE, OnCheckResultsForUnusedSpace)
+	ON_BN_CLICKED(IDC_CHECK_RESULTS_FOR_FILES, OnCheckResultsForFiles)
+	//}}AFX_MSG_MAP
 	ON_BN_CLICKED(IDC_BUTTON_PROTECTION, OnBnClickedButtonProtection)
 	ON_BN_CLICKED(IDC_BUTTON_HOTKEYS, OnBnClickedButtonHotkeys)
 END_MESSAGE_MAP()
@@ -98,14 +99,14 @@ END_MESSAGE_MAP()
 
 CSchedulerPreferencesPage::CSchedulerPreferencesPage() : CPropertyPage(CSchedulerPreferencesPage::IDD)
 {
-    //{{AFX_DATA_INIT(CSchedulerPreferencesPage)
-    m_bLog = FALSE;
-    m_bStartup = FALSE;
-    m_bNoVisualErrors = FALSE;
-    m_bLogOnlyErrors = FALSE;
-    m_dwMaxLogSize = 0;
-    m_bNoTrayIcon = FALSE;
-    m_bQueueTasks = FALSE;
+	//{{AFX_DATA_INIT(CSchedulerPreferencesPage)
+	m_bLog = FALSE;
+	m_bStartup = FALSE;
+	m_bNoVisualErrors = FALSE;
+	m_bLogOnlyErrors = FALSE;
+	m_dwMaxLogSize = 0;
+	m_bNoTrayIcon = FALSE;
+	m_bQueueTasks = FALSE;
 	m_bEnabled = FALSE;
 	m_bHideOnMinimize = FALSE;
 	//}}AFX_DATA_INIT
@@ -117,16 +118,16 @@ CSchedulerPreferencesPage::~CSchedulerPreferencesPage()
 
 void CSchedulerPreferencesPage::DoDataExchange(CDataExchange* pDX)
 {
-    CPropertyPage::DoDataExchange(pDX);
-    //{{AFX_DATA_MAP(CSchedulerPreferencesPage)
-    DDX_Control(pDX, IDC_SPIN_LIMIT, m_sbLimitSize);
-    DDX_Check(pDX, IDC_CHECK_LOG, m_bLog);
-    DDX_Check(pDX, IDC_CHECK_STARTUP, m_bStartup);
-    DDX_Check(pDX, IDC_CHECK_NOVISUALERRORS, m_bNoVisualErrors);
-    DDX_Check(pDX, IDC_CHECK_LOG_ONLYERRORS, m_bLogOnlyErrors);
-    DDX_Text(pDX, IDC_EDIT_LIMIT, m_dwMaxLogSize);
-    DDX_Check(pDX, IDC_CHECK_NOTRAYICON, m_bNoTrayIcon);
-    DDX_Check(pDX, IDC_CHECK_QUEUE, m_bQueueTasks);
+	CPropertyPage::DoDataExchange(pDX);
+	//{{AFX_DATA_MAP(CSchedulerPreferencesPage)
+	DDX_Control(pDX, IDC_SPIN_LIMIT, m_sbLimitSize);
+	DDX_Check(pDX, IDC_CHECK_LOG, m_bLog);
+	DDX_Check(pDX, IDC_CHECK_STARTUP, m_bStartup);
+	DDX_Check(pDX, IDC_CHECK_NOVISUALERRORS, m_bNoVisualErrors);
+	DDX_Check(pDX, IDC_CHECK_LOG_ONLYERRORS, m_bLogOnlyErrors);
+	DDX_Text(pDX, IDC_EDIT_LIMIT, m_dwMaxLogSize);
+	DDX_Check(pDX, IDC_CHECK_NOTRAYICON, m_bNoTrayIcon);
+	DDX_Check(pDX, IDC_CHECK_QUEUE, m_bQueueTasks);
 	DDX_Check(pDX, IDC_CHECK_ENABLE, m_bEnabled);
 	DDX_Check(pDX, IDC_CHECK_HIDEONMINIMIZE, m_bHideOnMinimize);
 	//}}AFX_DATA_MAP
@@ -134,9 +135,9 @@ void CSchedulerPreferencesPage::DoDataExchange(CDataExchange* pDX)
 
 
 BEGIN_MESSAGE_MAP(CSchedulerPreferencesPage, CPropertyPage)
-    //{{AFX_MSG_MAP(CSchedulerPreferencesPage)
-    ON_BN_CLICKED(IDC_CHECK_LOG_LIMITSIZE, OnCheckLogLimitsize)
-    ON_BN_CLICKED(IDC_CHECK_LOG, OnCheckLog)
+	//{{AFX_MSG_MAP(CSchedulerPreferencesPage)
+	ON_BN_CLICKED(IDC_CHECK_LOG_LIMITSIZE, OnCheckLogLimitsize)
+	ON_BN_CLICKED(IDC_CHECK_LOG, OnCheckLog)
 	ON_BN_CLICKED(IDC_CHECK_NOTRAYICON, OnCheckNotrayicon)
 	//}}AFX_MSG_MAP
 END_MESSAGE_MAP()
@@ -144,56 +145,56 @@ END_MESSAGE_MAP()
 
 static inline BOOL IsAdminOnNT()
 {
-    SC_HANDLE hSC;
+	SC_HANDLE hSC;
 
-    // Try an Admin Privileged API - if it works return
-    // TRUE - else FALSE
+	// Try an Admin Privileged API - if it works return
+	// TRUE - else FALSE
 
-    hSC = OpenSCManager(NULL, NULL,
-                        GENERIC_READ | GENERIC_WRITE | GENERIC_EXECUTE);
+	hSC = OpenSCManager(NULL, NULL,
+		GENERIC_READ | GENERIC_WRITE | GENERIC_EXECUTE);
 
-    if (hSC == NULL)
-        return FALSE;
+	if (hSC == NULL)
+		return FALSE;
 
-    CloseServiceHandle(hSC);
-    return TRUE;
+	CloseServiceHandle(hSC);
+	return TRUE;
 }
 
 BOOL CEraserPreferencesPage::OnInitDialog()
 {
-    CPropertyPage::OnInitDialog();
+	CPropertyPage::OnInitDialog();
 
-    // Clearing Paging file is a Windows NT security
-    // feature and not available on Windows 9x
+	// Clearing Paging file is a Windows NT security
+	// feature and not available on Windows 9x
 
-    OSVERSIONINFO ov;
+	OSVERSIONINFO ov;
 
-    ZeroMemory(&ov, sizeof(OSVERSIONINFO));
-    ov.dwOSVersionInfoSize = sizeof(OSVERSIONINFO);
+	ZeroMemory(&ov, sizeof(OSVERSIONINFO));
+	ov.dwOSVersionInfoSize = sizeof(OSVERSIONINFO);
 
-    VERIFY(GetVersionEx(&ov));
+	VERIFY(GetVersionEx(&ov));
 
-    CButton *pButtonSwap = (CButton*)GetDlgItem(IDC_CHECK_CLEAR_SWAP);
+	CButton *pButtonSwap = (CButton*)GetDlgItem(IDC_CHECK_CLEAR_SWAP);
 
-    if (ov.dwPlatformId == VER_PLATFORM_WIN32_NT)
-    {
-        if (IsAdminOnNT())
-        {
-            pButtonSwap->EnableWindow(TRUE);
-        }
-    }
-    else
-    {
-        m_bClearSwap = FALSE;
-        pButtonSwap->SetCheck(0);
-    }
+	if (ov.dwPlatformId == VER_PLATFORM_WIN32_NT)
+	{
+		if (IsAdminOnNT())
+		{
+			pButtonSwap->EnableWindow(TRUE);
+		}
+	}
+	else
+	{
+		m_bClearSwap = FALSE;
+		pButtonSwap->SetCheck(0);
+	}
 
 
-    BOOL bEnable = (m_bResultsForFiles || m_bResultsForUnusedSpace);
+	BOOL bEnable = (m_bResultsForFiles || m_bResultsForUnusedSpace);
 
-    GetDlgItem(IDC_CHECK_RESULTSONLYWHENFAILED)->EnableWindow(bEnable);
-    GetDlgItem(IDC_CHECK_SHELLEXT_RESULTS)->EnableWindow(bEnable);
-	
+	GetDlgItem(IDC_CHECK_RESULTSONLYWHENFAILED)->EnableWindow(bEnable);
+	GetDlgItem(IDC_CHECK_SHELLEXT_RESULTS)->EnableWindow(bEnable);
+
 
 	CString strButtonTitle;
 	strButtonTitle.LoadString(CSecurityManager::IsProtected() ? IDS_CLEAR_PROTECTION : IDS_SET_PROTECTION);
@@ -201,101 +202,101 @@ BOOL CEraserPreferencesPage::OnInitDialog()
 	GetDlgItem(IDC_BUTTON_PROTECTION)->SetWindowText(strButtonTitle);
 
 
-    return TRUE;  // return TRUE unless you set the focus to a control
-                  // EXCEPTION: OCX Property Pages should return FALSE
+	return TRUE;  // return TRUE unless you set the focus to a control
+	// EXCEPTION: OCX Property Pages should return FALSE
 }
 
 void CSchedulerPreferencesPage::OnCheckLogLimitsize()
 {
-    CButton *pCheck = static_cast<CButton*>(GetDlgItem(IDC_CHECK_LOG_LIMITSIZE));
+	CButton *pCheck = static_cast<CButton*>(GetDlgItem(IDC_CHECK_LOG_LIMITSIZE));
 
-    if (pCheck->GetCheck())
-    {
-        GetDlgItem(IDC_EDIT_LIMIT)->EnableWindow(TRUE);
-        m_sbLimitSize.EnableWindow(TRUE);
-    }
-    else
-    {
-        GetDlgItem(IDC_EDIT_LIMIT)->EnableWindow(FALSE);
-        m_sbLimitSize.EnableWindow(FALSE);
-    }
+	if (pCheck->GetCheck())
+	{
+		GetDlgItem(IDC_EDIT_LIMIT)->EnableWindow(TRUE);
+		m_sbLimitSize.EnableWindow(TRUE);
+	}
+	else
+	{
+		GetDlgItem(IDC_EDIT_LIMIT)->EnableWindow(FALSE);
+		m_sbLimitSize.EnableWindow(FALSE);
+	}
 }
 
 void CSchedulerPreferencesPage::OnCheckLog()
 {
-    UpdateData(TRUE);
-    GetDlgItem(IDC_CHECK_LOG_ONLYERRORS)->EnableWindow(m_bLog);
+	UpdateData(TRUE);
+	GetDlgItem(IDC_CHECK_LOG_ONLYERRORS)->EnableWindow(m_bLog);
 
-    CButton *pCheck = static_cast<CButton*>(GetDlgItem(IDC_CHECK_LOG_LIMITSIZE));
+	CButton *pCheck = static_cast<CButton*>(GetDlgItem(IDC_CHECK_LOG_LIMITSIZE));
 
-    GetDlgItem(IDC_EDIT_LIMIT)->EnableWindow(pCheck->GetCheck() && m_bLog);
-    m_sbLimitSize.EnableWindow(pCheck->GetCheck() && m_bLog);
+	GetDlgItem(IDC_EDIT_LIMIT)->EnableWindow(pCheck->GetCheck() && m_bLog);
+	m_sbLimitSize.EnableWindow(pCheck->GetCheck() && m_bLog);
 
-    pCheck->EnableWindow(m_bLog);
-    GetDlgItem(IDC_STATIC_KB)->EnableWindow(m_bLog);
+	pCheck->EnableWindow(m_bLog);
+	GetDlgItem(IDC_STATIC_KB)->EnableWindow(m_bLog);
 }
 
 BOOL CSchedulerPreferencesPage::OnInitDialog()
 {
-    CPropertyPage::OnInitDialog();
+	CPropertyPage::OnInitDialog();
 
-    GetDlgItem(IDC_CHECK_LOG_ONLYERRORS)->EnableWindow(m_bLog);
+	GetDlgItem(IDC_CHECK_LOG_ONLYERRORS)->EnableWindow(m_bLog);
 
-    m_sbLimitSize.SetBase(10);
-    m_sbLimitSize.SetRange(1, 1024);
-    m_sbLimitSize.SetPos(m_dwMaxLogSize);
-    m_sbLimitSize.SetBuddy(GetDlgItem(IDC_EDIT_LIMIT));
+	m_sbLimitSize.SetBase(10);
+	m_sbLimitSize.SetRange(1, 1024);
+	m_sbLimitSize.SetPos(m_dwMaxLogSize);
+	m_sbLimitSize.SetBuddy(GetDlgItem(IDC_EDIT_LIMIT));
 
-    CButton *pCheck = static_cast<CButton*>(GetDlgItem(IDC_CHECK_LOG_LIMITSIZE));
+	CButton *pCheck = static_cast<CButton*>(GetDlgItem(IDC_CHECK_LOG_LIMITSIZE));
 
-    if (m_dwMaxLogSize < 1)
-    {
-        m_dwMaxLogSize = 1;
-        pCheck->SetCheck(0);
-    }
-    else
-    {
-        pCheck->SetCheck(1);
-    }
+	if (m_dwMaxLogSize < 1)
+	{
+		m_dwMaxLogSize = 1;
+		pCheck->SetCheck(0);
+	}
+	else
+	{
+		pCheck->SetCheck(1);
+	}
 
-    GetDlgItem(IDC_EDIT_LIMIT)->EnableWindow(pCheck->GetCheck() && m_bLog);
-    m_sbLimitSize.EnableWindow(pCheck->GetCheck() && m_bLog);
+	GetDlgItem(IDC_EDIT_LIMIT)->EnableWindow(pCheck->GetCheck() && m_bLog);
+	m_sbLimitSize.EnableWindow(pCheck->GetCheck() && m_bLog);
 
-    pCheck->EnableWindow(m_bLog);
-    GetDlgItem(IDC_STATIC_KB)->EnableWindow(m_bLog);
+	pCheck->EnableWindow(m_bLog);
+	GetDlgItem(IDC_STATIC_KB)->EnableWindow(m_bLog);
 
-    if (m_bNoTrayIcon)
-    {
-        m_bHideOnMinimize = FALSE;
-        GetDlgItem(IDC_CHECK_HIDEONMINIMIZE)->EnableWindow(FALSE);
-    }
+	if (m_bNoTrayIcon)
+	{
+		m_bHideOnMinimize = FALSE;
+		GetDlgItem(IDC_CHECK_HIDEONMINIMIZE)->EnableWindow(FALSE);
+	}
 
 
-	
-		
-    UpdateData(FALSE);
 
-    return TRUE;  // return TRUE unless you set the focus to a control
-                  // EXCEPTION: OCX Property Pages should return FALSE
+
+	UpdateData(FALSE);
+
+	return TRUE;  // return TRUE unless you set the focus to a control
+	// EXCEPTION: OCX Property Pages should return FALSE
 }
 
 void CSchedulerPreferencesPage::OnOK()
 {
-    CPropertyPage::OnOK();
+	CPropertyPage::OnOK();
 
-    CButton *pCheck = static_cast<CButton*>(GetDlgItem(IDC_CHECK_LOG_LIMITSIZE));
+	CButton *pCheck = static_cast<CButton*>(GetDlgItem(IDC_CHECK_LOG_LIMITSIZE));
 
-    if (!pCheck->GetCheck())
-        m_dwMaxLogSize = 0;
+	if (!pCheck->GetCheck())
+		m_dwMaxLogSize = 0;
 }
 
 void CEraserPreferencesPage::OnCheckResultsForUnusedSpace()
 {
-    UpdateData(TRUE);
-    BOOL bEnable = (m_bResultsForFiles || m_bResultsForUnusedSpace);
+	UpdateData(TRUE);
+	BOOL bEnable = (m_bResultsForFiles || m_bResultsForUnusedSpace);
 
-    GetDlgItem(IDC_CHECK_RESULTSONLYWHENFAILED)->EnableWindow(bEnable);
-    GetDlgItem(IDC_CHECK_SHELLEXT_RESULTS)->EnableWindow(bEnable);
+	GetDlgItem(IDC_CHECK_RESULTSONLYWHENFAILED)->EnableWindow(bEnable);
+	GetDlgItem(IDC_CHECK_SHELLEXT_RESULTS)->EnableWindow(bEnable);
 	if (!bEnable)
 	{
 		CButton* btn = (CButton*)GetDlgItem(IDC_CHECK_RESULTSONLYWHENFAILED);
@@ -307,11 +308,11 @@ void CEraserPreferencesPage::OnCheckResultsForUnusedSpace()
 
 void CEraserPreferencesPage::OnCheckResultsForFiles()
 {
-    UpdateData(TRUE);
-    BOOL bEnable = (m_bResultsForFiles || m_bResultsForUnusedSpace);
+	UpdateData(TRUE);
+	BOOL bEnable = (m_bResultsForFiles || m_bResultsForUnusedSpace);
 
-    GetDlgItem(IDC_CHECK_RESULTSONLYWHENFAILED)->EnableWindow(bEnable);
-    GetDlgItem(IDC_CHECK_SHELLEXT_RESULTS)->EnableWindow(bEnable);
+	GetDlgItem(IDC_CHECK_RESULTSONLYWHENFAILED)->EnableWindow(bEnable);
+	GetDlgItem(IDC_CHECK_SHELLEXT_RESULTS)->EnableWindow(bEnable);
 	if (!bEnable)
 	{
 		CButton* btn = (CButton*)GetDlgItem(IDC_CHECK_RESULTSONLYWHENFAILED);
@@ -324,14 +325,14 @@ void CEraserPreferencesPage::OnCheckResultsForFiles()
 void CSchedulerPreferencesPage::OnCheckNotrayicon() 
 {
 	UpdateData(TRUE);
-    GetDlgItem(IDC_CHECK_HIDEONMINIMIZE)->EnableWindow(!m_bNoTrayIcon);
+	GetDlgItem(IDC_CHECK_HIDEONMINIMIZE)->EnableWindow(!m_bNoTrayIcon);
 }
 
 void CEraserPreferencesPage::OnBnClickedButtonProtection()
 {
 	bool res;
 	CString strButtonTitle;
-		
+
 	if (CSecurityManager::IsProtected())
 	{
 		res = ClearProtection();
