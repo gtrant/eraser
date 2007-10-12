8a3fdc4a371b660eb4f254be76b85ae2661b512c
lowjoel <lowjoel@5a19fcc5-093c-0410-9d93-cd3e460611b7>
2007-10-12 08:08:29 +0000

    Bump the version number.

--- version.h
+++ version.h
@@ -2,6 +2,6 @@
 #define MAJOR_NUMBER 5
 #define MINOR_NUMBER 8
 #define BUILD_NUMBER 5
-#define BUILD_NUMBER_STRING "beta1-1"
-#define VERSION_NUMBER_STRING "5.85-beta1"
+#define BUILD_NUMBER_STRING "beta2-1"
+#define VERSION_NUMBER_STRING "5.85-beta2"
 #define COMPANY_NAME "Heidi Computers Ltd"
