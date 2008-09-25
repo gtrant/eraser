4d180c9ee0b3926c37f1ff58841b7fd13d917bc4
lowjoel <lowjoel@5a19fcc5-093c-0410-9d93-cd3e460611b7>
2008-09-25 12:30:01 +0000

    Fixes #42. Apparently MFC changed sizes of the structures causing problems. This stdafx is cannibalised from the EraserDll one.

--- EraserUI/stdafx.h
+++ EraserUI/stdafx.h
@@ -1,43 +1,100 @@
 // stdafx.h : include file for standard system include files,
-// or project specific include files that are used frequently, but
-// are changed infrequently
+//  or project specific include files that are used frequently, but
+//      are changed infrequently
 //
+// Eraser. Secure data removal. For Windows.
+// Copyright © 1997-2001  Sami Tolvanen (sami@tolvanen.com).
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA
+// 02111-1307, USA.
 
+#ifdef _UNICODE
+#error UNICODE is not supported!
+#endif
+
+#undef NOCRYPT
 #pragma once
 
-// Modify the following defines if you have to target a platform prior to the ones specified below.
-// Refer to MSDN for the latest info on corresponding values for different platforms.
-#ifndef WINVER				// Allow use of features specific to Windows XP or later.
-#define WINVER 0x0501		// Change this to the appropriate value to target other versions of Windows.
-#endif
-
-#ifndef _WIN32_WINNT		// Allow use of features specific to Windows XP or later.                   
-#define _WIN32_WINNT 0x0501	// Change this to the appropriate value to target other versions of Windows.
-#endif						
-
-#ifndef _WIN32_WINDOWS		// Allow use of features specific to Windows 98 or later.
-#define _WIN32_WINDOWS 0x0410 // Change this to the appropriate value to target Windows Me or later.
-#endif
-
-#ifndef _WIN32_IE			// Allow use of features specific to IE 6.0 or later.
-#define _WIN32_IE 0x0600	// Change this to the appropriate value to target other versions of IE.
-#endif
-
-#define WIN32_LEAN_AND_MEAN		// Exclude rarely-used stuff from Windows headers
-#define _ATL_CSTRING_EXPLICIT_CONSTRUCTORS	// some CString constructors will be explicit
-
 #ifndef VC_EXTRALEAN
-#define VC_EXTRALEAN		// Exclude rarely-used stuff from Windows headers
+#define VC_EXTRALEAN	// Exclude rarely-used items from Windows headers.
 #endif
 
-#include <afx.h>
+// Modify the following defines if you have to target an OS before the ones 
+// specified in the following code. See MSDN for the latest information
+// about corresponding values for different operating systems.
+#ifndef WINVER		// Permit use of features specific to Windows 95 and Windows NT 4.0 or later.
+#define WINVER 0x0400	// Change this to the appropriate value to target 
+#endif                     // Windows 98 and Windows 2000 or later.
+
+#ifndef _WIN32_WINNT	// Permit use of features specific to Windows NT 4.0 or later.
+#define _WIN32_WINNT 0x0400	// Change this to the appropriate value to target 
+#endif		         // Windows 98 and Windows 2000 or later.			
+
+#ifndef _WIN32_WINDOWS	      // Permit use of features specific to Windows 98 or later.
+#define _WIN32_WINDOWS 0x0410    // Change this to the appropriate value to target 
+#endif			      // Windows Millennium Edition or later.
+
+#ifndef _WIN32_IE		// Permit use of features specific to Internet Explorer 4.0 or later.
+#define _WIN32_IE 0x0400   // Change this to the appropriate value to target 
+#endif			// Internet Explorer 5.0 or later.
+
+#define _ATL_CSTRING_EXPLICIT_CONSTRUCTORS  // Some CString constructors will be explicit.
+
+// Turns off MFC feature that hides of some common warning messages
+// that are frequently and safely ignored .
+#define _AFX_ALL_WARNINGS
 #include <afxwin.h>         // MFC core and standard components
 #include <afxext.h>         // MFC extensions
+
+#ifndef _AFX_NO_OLE_SUPPORT
+#include <afxole.h>         // MFC OLE classes
+#include <afxodlgs.h>       // MFC OLE dialog classes
+#include <afxdisp.h>        // MFC OLE automation classes
+#endif // _AFX_NO_OLE_SUPPORT
+
+#define _AFX_NO_DB_SUPPORT
+
+#ifndef _AFX_NO_DB_SUPPORT
+#include <afxdb.h>          // MFC ODBC database classes
+#endif // _AFX_NO_DB_SUPPORT
+
+#define _AFX_NO_DAO_SUPPORT
+
+#ifndef _AFX_NO_DAO_SUPPORT
+#include <afxdao.h>         // MFC DAO database classes
+#endif // _AFX_NO_DAO_SUPPORT
+
 #ifndef _AFX_NO_AFXCMN_SUPPORT
-#include <afxcmn.h>         // MFC support for Windows Common Controls
+#include <afxcmn.h>         // MFC support for Windows 95 Common Controls
 #endif // _AFX_NO_AFXCMN_SUPPORT
 #include <afxcview.h>
-#include <afxole.h>
 
 #include <afxmt.h>
+#include <afxtempl.h>
+#include <afxcoll.h>
+#include <eh.h>                       // structured exception
+#include "..\shared\SeException.h"    // handling
+#include <Shlwapi.h>
+
+#ifndef _AFXDLL
+#define AfxLoadLibrary  ::LoadLibrary
+#define AfxFreeLibrary  ::FreeLibrary
+#endif
+
+// use intrinsic memory functions
+#pragma intrinsic(memset, memcpy)
+
 #include <lm.h>
+#include <shlobj.h>
