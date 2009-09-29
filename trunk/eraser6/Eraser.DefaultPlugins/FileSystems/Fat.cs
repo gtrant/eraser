36bbe7c7e366a4a65fe3f286799b7c1fc79c2315
lowjoel <lowjoel@5a19fcc5-093c-0410-9d93-cd3e460611b7>
2009-09-29 13:46:01 +0000

    Implemented erasing directory structures (for FAT32)

--- trunk/eraser6/Eraser.DefaultPlugins/FileSystems/Fat.cs
+++ trunk/eraser6/Eraser.DefaultPlugins/FileSystems/Fat.cs
@@ -91,7 +91,45 @@ namespace Eraser.DefaultPlugins
 		public override void EraseDirectoryStructures(VolumeInfo info,
 			FileSystemEntriesEraseProgress callback)
 		{
-			Fat32Api api = new Fat32Api(info);
+			using (FileStream stream = info.Open(FileAccess.ReadWrite, FileShare.ReadWrite))
+			{
+				int directoriesCleaned = 0;
+				Fat32Api api = new Fat32Api(info, stream);
+				HashSet<uint> eraseQueueClusters = new HashSet<uint>();
+				List<FatDirectoryEntry> eraseQueue = new List<FatDirectoryEntry>();
+				{
+					FatDirectoryEntry entry = api.LoadDirectory(string.Empty);
+					eraseQueue.Add(entry);
+					eraseQueueClusters.Add(entry.Cluster);
+				}
+
+				using (VolumeLock volumeLock = info.LockVolume(stream))
+				{
+					while (eraseQueue.Count != 0)
+					{
+						if (callback != null)
+							callback(directoriesCleaned, directoriesCleaned + eraseQueue.Count);
+
+						FatDirectory currentDir = api.LoadDirectory(eraseQueue[0].FullName);
+						eraseQueue.RemoveAt(0);
+
+						//Queue the subfolders in this directory
+						foreach (KeyValuePair<string, FatDirectoryEntry> entry in currentDir.Items)
+							if (entry.Value.Type == FatDirectoryEntryTypes.Directory)
+							{
+								//Check that we don't have the same cluster queued twice (e.g. for
+								//long/8.3 file names)
+								if (eraseQueueClusters.Contains(entry.Value.Cluster))
+									continue;
+
+								eraseQueueClusters.Add(entry.Value.Cluster);
+								eraseQueue.Add(entry.Value);
+							}
+
+						currentDir.ClearDeletedEntries();
+					}
+				}
+			}
 		}
 	}
 }
