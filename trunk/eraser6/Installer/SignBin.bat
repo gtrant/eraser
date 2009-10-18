0487f5e5f985bde6354d7987c73ee9508548b8c8
lowjoel <lowjoel@5a19fcc5-093c-0410-9d93-cd3e460611b7>
2009-10-18 07:10:22 +0000

    List the binaries to sign dynamically so we can separately build win32 and x64 binaries.

--- trunk/eraser6/Installer/SignBin.bat
+++ trunk/eraser6/Installer/SignBin.bat
@@ -7,14 +7,7 @@ GOTO :eof
 
 :Sign
 @rem Core binaries
-signtool sign /a ^
- /t http://timestamp.verisign.com/scripts/timestamp.dll "%~1\bin\Release\Eraser.exe"^
- "%~1\bin\Release\Eraser.Manager.dll" "%~1\bin\Release\Eraser.Util.dll"^
- "%~1\bin\Release\Plugins\Eraser.DefaultPlugins.dll" ^
- "%~1\bin\Release\x64\Eraser.Shell.dll" "%~1\bin\Release\Win32\Eraser.Shell.dll" ^
- "%~1\bin\Release\x64\Eraser.Unlocker.dll" "%~1\bin\Release\Win32\Eraser.Unlocker.dll" ^
- ^
- "%~1\bin\Release\en\Eraser.Manager.resources.dll" "%~1\bin\Release\en\Eraser.resources.dll"^
- "%~1\bin\Release\nl\Eraser.Manager.resources.dll" "%~1\bin\Release\nl\Eraser.resources.dll"^
- "%~1\bin\Release\Plugins\en\Eraser.DefaultPlugins.resources.dll"^
- "%~1\bin\Release\Plugins\nl\Eraser.DefaultPlugins.resources.dll"
+setlocal EnableDelayedExpansion 
+for /r %%i in (%1\bin\Release\*.dll) do set binaries=!binaries! %%i
+for /r %%i in (%1\bin\Release\*.exe) do set binaries=!binaries! %%i
+signtool sign /a  /t http://timestamp.verisign.com/scripts/timestamp.dll !binaries!
