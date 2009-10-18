22b9797f32696ca78bae7f184293b2beb5d49f0e
lowjoel <lowjoel@5a19fcc5-093c-0410-9d93-cd3e460611b7>
2009-10-18 11:53:44 +0000

    Sign all binaries except vshost.exe

--- trunk/eraser6/Installer/SignBin.bat
+++ trunk/eraser6/Installer/SignBin.bat
@@ -1,13 +1,10 @@
-goto :Sign
-
-:: pass params to a subroutine
-SET _params=%*
-CALL :Sign "%_params%"
-GOTO :eof
-
-:Sign
-@rem Core binaries
 setlocal EnableDelayedExpansion 
-for /r %%i in (%1\bin\Release\*.dll) do set binaries=!binaries! %%i
-for /r %%i in (%1\bin\Release\*.exe) do set binaries=!binaries! %%i
-signtool sign /a  /t http://timestamp.verisign.com/scripts/timestamp.dll !binaries!
+for /r %%i in (%1\bin\Release\*.dll) do set binaries=!binaries! "%%i"
+for /r %%i in (%1\bin\Release\*.exe) do (
+	set j=%%i
+	if "!j:~-10!" neq "vshost.exe" (
+		set binaries=!binaries! "%%i"
+	)
+)
+
+signtool sign /a /t http://timestamp.verisign.com/scripts/timestamp.dll !binaries!
