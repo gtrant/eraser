5784bce87c501ebf960217443c0ecc4e815f29de
lowjoel <lowjoel@5a19fcc5-093c-0410-9d93-cd3e460611b7>
2010-04-16 13:03:12 +0000

    Improved on the Download class to be usable for Update querying as well, and upgrade the UpdateList classes to use the class directly instead of querying the database. This removes one more dependency on the traditional mysql PHP functions.

--- website/scripts/updates.php
+++ website/scripts/updates.php
@@ -1,10 +1,39 @@
 <?php
+require_once('Database.php');
+require_once('Download.php');
+
+/**
+ * Base class for all update lists types.
+ */
 abstract class UpdateListBase
 {
+	/**
+	 * Gets the version of the update list generated.
+	 *
+	 * @return string
+	 */
 	protected abstract function GetVersion();
+
+	/**
+	 * Gets the updates that the client may require.
+	 *
+	 * @param string $clientVersion The version string of the client.
+	 * @return array                An array of Download objects which will be presented to the client.
+	 */
 	protected abstract function GetUpdates($clientVersion);
+
+	/**
+	 * Triggers the generation of the update body for the client.
+	 *
+	 * @param string $clientVersion The version string of the client.
+	 */
 	protected abstract function OutputBodyXml($clientVersion);
-	
+
+	/**
+	 * Generates an update list for a client with the given version string.
+	 *
+	 * @param string $clientVersion The version string of the client.
+	 */
 	public function Output($clientVersion)
 	{
 		$this->OutputHeader();
@@ -21,30 +50,24 @@ abstract class UpdateListBase
 	
 	protected function OutputUpdates($clientVersion)
 	{
-		$query = $this->GetUpdates($clientVersion);
+		$updates = $this->GetUpdates($clientVersion);
 
 		$lastItemType = null;
-		while ($row = mysql_fetch_array($query))
+		foreach ($updates as $update)
 		{
-			if ($row['Type'] != $lastItemType)
+			//Output the closing type tag and open a new type tag for the new update type.
+			if ($update->Type != $lastItemType)
 			{
 				if ($lastItemType !== null)
 					printf('	</%s>' . "\n", $lastItemType);
-				printf('	<%s>' . "\n", $row['Type']);
-				$lastItemType = $row['Type'];
+				printf('	<%s>' . "\n", $update->Type);
+				$lastItemType = $update->Type;
 			}
-		
-			//Get the link to the download. We got three forms, relative, absolute and query.
-			//Relative links are mirrored by SF.
-			//Absolute links... are absolute links.
-			//Query links are prefixed with ?, they are handled by download.php on the Eraser website.
-			if (substr($row['Link'], 0, 1) == '?')
-				$link = 'http://' . $_SERVER['SERVER_NAME'] . '/download.php?id=' . $row['DownloadID'];
-			else
-				$link = $row['Link'];
+
+			//Print the download entry.
 			printf('		<item name="%s" version="%s" publisher="%s" architecture="%s" filesize="%d">%s</item>
-', htmlentities($row['Name']), $row['Version'], htmlentities($row['PublisherName']), $row['Architecture'],
-				$row['Filesize'], htmlentities($link));
+', htmlentities($update->Name), $update->Version, htmlentities($update->Publisher->Name), $update->Architecture,
+				$update->Filesize, htmlentities($update->GetDisplayedLink()));
 		}
 		
 		if (!empty($lastItemType))
@@ -67,35 +90,46 @@ class UpdateList1 extends UpdateListBase
 	protected function GetUpdates($clientVersion)
 	{
 		//Prepare the list of updates
-		return mysql_query(sprintf('SELECT downloads.*, publishers.Name as PublisherName
+		$pdo = new Database();
+		$statement = $pdo->prepare('SELECT DownloadID
 			FROM downloads
-			INNER JOIN publishers ON
-				downloads.PublisherID=publishers.PublisherID
 			WHERE
 				(Superseded = 0) AND
 				(`Type` <> \'build\') AND
 				(
 					(MinVersion IS NULL AND MaxVersion IS NULL) OR
-					(MinVersion IS NULL AND MaxVersion > \'%1$s\') OR
+					(MinVersion IS NULL AND MaxVersion > :Version) OR
 					(MinVersion <= \'%1$s\' AND MaxVersion IS NULL) OR
-					(MinVersion <= \'%1$s\' AND MaxVersion > \'%1$s\')
+					(MinVersion <= \'%1$s\' AND MaxVersion > :Version)
 				)
-			ORDER BY `Type` ASC', mysql_real_escape_string($clientVersion)));
+			ORDER BY `Type` ASC');
+		$statement->bindParam('Version', $clientVersion);
+		$statement->execute();
+
+		$result = array();
+		$downloadId = null;
+		$statement->bindColumn('DownloadID', $downloadId);
+		while ($statement->fetch())
+			$result[] = new Download($downloadId);
+
+		return $result;
 	}
 	
 	private function OutputMirrors()
 	{
 		//Output the list of mirrors
-		$query = mysql_query('SELECT * FROM mirrors ORDER By Continent, Country, City');
+		$pdo = new Database();
+		$statement = $pdo->query('SELECT * FROM mirrors ORDER By Continent, Country, City');
 		echo '	<mirrors>
 		<mirror location="(automatically decide)">http://downloads.sourceforge.net/eraser/</mirror>
 ';
-		while ($row = mysql_fetch_array($query))
-		{
-			printf('		<mirror location="%s, %s">%s</mirror>' . "\n", $row['City'], $row['Country'],
-				$row['URL']);
-		}
-		echo '	</mirrors>';
+
+		$statement->bindColumn('City', $city);
+		$statement->bindColumn('Country', $country);
+		$statement->bindColumn('URL', $url);
+		while ($statement->fetch())
+			printf('		<mirror location="%s, %s">%s</mirror>' . "\n", $city, $country, $url);
+		echo '	</mirrors>' . "\n";
 	}
 
 	protected function OutputBodyXml($clientVersion)
@@ -115,26 +149,35 @@ class UpdateList1_1 extends UpdateListBase
 	protected function GetUpdates($clientVersion)
 	{
 		//Prepare the list of updates
-		return mysql_query(sprintf('SELECT downloads.*, publishers.Name as PublisherName
+		$pdo = new Database();
+		$statement = $pdo->prepare('SELECT DownloadID
 			FROM downloads
-			INNER JOIN publishers ON
-				downloads.PublisherID=publishers.PublisherID
 			WHERE
 				(Superseded = 0) AND
 				(
 					(															-- Program Updates
 						(`Type` <> \'build\') AND
 						(MinVersion IS NULL AND MaxVersion IS NULL) OR
-						(MinVersion IS NULL AND MaxVersion > \'%1$s\') OR
+						(MinVersion IS NULL AND MaxVersion > :Version) OR
 						(MinVersion <= \'%1$s\' AND MaxVersion IS NULL) OR
-						(MinVersion <= \'%1$s\' AND MaxVersion > \'%1$s\')
+						(MinVersion <= \'%1$s\' AND MaxVersion > :Version)
 					) OR
 					(															-- Nightly builds greater than our version
 						(`Type` = \'build\') AND
-						(Version > \'%1$s\')
+						(Version > :Version)
 					)
 				)
-			ORDER BY `Type` ASC', mysql_real_escape_string($clientVersion)));
+			ORDER BY `Type` ASC');
+		$statement->bindParam('Version', $clientVersion);
+		$statement->execute();
+
+		$result = array();
+		$downloadId = null;
+		$statement->bindColumn('DownloadID', $downloadId);
+		while ($statement->fetch())
+			$result[] = new Download($downloadId);
+
+		return $result;
 	}
 	
 	protected function OutputBodyXml($clientVersion)
