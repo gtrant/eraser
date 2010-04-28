acff1a823e86227a7aa5451ded5560f098fdd151
lowjoel <lowjoel@5a19fcc5-093c-0410-9d93-cd3e460611b7>
2010-04-28 08:25:36 +0000

    More detailed error report message and explanatory note.

--- trunk/eraser/Eraser.BlackBox/BlackBoxMainForm.Designer.cs
+++ trunk/eraser/Eraser.BlackBox/BlackBoxMainForm.Designer.cs
@@ -49,105 +49,82 @@ namespace Eraser.BlackBox
 		/// </summary>
 		private void InitializeComponent()
 		{
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(BlackBoxMainForm));
-            this.MainLbl = new System.Windows.Forms.Label();
-            this.SubmitBtn = new System.Windows.Forms.Button();
-            this.PostponeBtn = new System.Windows.Forms.Button();
-            this.BlackBoxPic = new System.Windows.Forms.PictureBox();
-            this.ReportsLv = new System.Windows.Forms.ListView();
-            this.ReportsLvTimestampColumn = new System.Windows.Forms.ColumnHeader();
-            this.ReportsLvErrorColumn = new System.Windows.Forms.ColumnHeader();
-            ((System.ComponentModel.ISupportInitialize)(this.BlackBoxPic)).BeginInit();
-            this.SuspendLayout();
-            // 
-            // MainLbl
-            // 
-            this.MainLbl.AccessibleDescription = null;
-            this.MainLbl.AccessibleName = null;
-            resources.ApplyResources(this.MainLbl, "MainLbl");
-            this.MainLbl.Font = null;
-            this.MainLbl.Name = "MainLbl";
-            // 
-            // SubmitBtn
-            // 
-            this.SubmitBtn.AccessibleDescription = null;
-            this.SubmitBtn.AccessibleName = null;
-            resources.ApplyResources(this.SubmitBtn, "SubmitBtn");
-            this.SubmitBtn.BackgroundImage = null;
-            this.SubmitBtn.Font = null;
-            this.SubmitBtn.Name = "SubmitBtn";
-            this.SubmitBtn.UseVisualStyleBackColor = true;
-            this.SubmitBtn.Click += new System.EventHandler(this.SubmitBtn_Click);
-            // 
-            // PostponeBtn
-            // 
-            this.PostponeBtn.AccessibleDescription = null;
-            this.PostponeBtn.AccessibleName = null;
-            resources.ApplyResources(this.PostponeBtn, "PostponeBtn");
-            this.PostponeBtn.BackgroundImage = null;
-            this.PostponeBtn.Font = null;
-            this.PostponeBtn.Name = "PostponeBtn";
-            this.PostponeBtn.UseVisualStyleBackColor = true;
-            this.PostponeBtn.Click += new System.EventHandler(this.PostponeBtn_Click);
-            // 
-            // BlackBoxPic
-            // 
-            this.BlackBoxPic.AccessibleDescription = null;
-            this.BlackBoxPic.AccessibleName = null;
-            resources.ApplyResources(this.BlackBoxPic, "BlackBoxPic");
-            this.BlackBoxPic.BackgroundImage = null;
-            this.BlackBoxPic.Font = null;
-            this.BlackBoxPic.Image = global::Eraser.BlackBox.Properties.Resources.BlackBox;
-            this.BlackBoxPic.ImageLocation = null;
-            this.BlackBoxPic.Name = "BlackBoxPic";
-            this.BlackBoxPic.TabStop = false;
-            // 
-            // ReportsLv
-            // 
-            this.ReportsLv.AccessibleDescription = null;
-            this.ReportsLv.AccessibleName = null;
-            resources.ApplyResources(this.ReportsLv, "ReportsLv");
-            this.ReportsLv.BackgroundImage = null;
-            this.ReportsLv.CheckBoxes = true;
-            this.ReportsLv.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(BlackBoxMainForm));
+			this.MainLbl = new System.Windows.Forms.Label();
+			this.SubmitBtn = new System.Windows.Forms.Button();
+			this.PostponeBtn = new System.Windows.Forms.Button();
+			this.BlackBoxPic = new System.Windows.Forms.PictureBox();
+			this.ReportsLv = new System.Windows.Forms.ListView();
+			this.ReportsLvTimestampColumn = new System.Windows.Forms.ColumnHeader();
+			this.ReportsLvErrorColumn = new System.Windows.Forms.ColumnHeader();
+			((System.ComponentModel.ISupportInitialize)(this.BlackBoxPic)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// MainLbl
+			// 
+			resources.ApplyResources(this.MainLbl, "MainLbl");
+			this.MainLbl.Name = "MainLbl";
+			// 
+			// SubmitBtn
+			// 
+			resources.ApplyResources(this.SubmitBtn, "SubmitBtn");
+			this.SubmitBtn.Name = "SubmitBtn";
+			this.SubmitBtn.UseVisualStyleBackColor = true;
+			this.SubmitBtn.Click += new System.EventHandler(this.SubmitBtn_Click);
+			// 
+			// PostponeBtn
+			// 
+			resources.ApplyResources(this.PostponeBtn, "PostponeBtn");
+			this.PostponeBtn.Name = "PostponeBtn";
+			this.PostponeBtn.UseVisualStyleBackColor = true;
+			this.PostponeBtn.Click += new System.EventHandler(this.PostponeBtn_Click);
+			// 
+			// BlackBoxPic
+			// 
+			this.BlackBoxPic.Image = global::Eraser.BlackBox.Properties.Resources.BlackBox;
+			resources.ApplyResources(this.BlackBoxPic, "BlackBoxPic");
+			this.BlackBoxPic.Name = "BlackBoxPic";
+			this.BlackBoxPic.TabStop = false;
+			// 
+			// ReportsLv
+			// 
+			resources.ApplyResources(this.ReportsLv, "ReportsLv");
+			this.ReportsLv.CheckBoxes = true;
+			this.ReportsLv.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
             this.ReportsLvTimestampColumn,
             this.ReportsLvErrorColumn});
-            this.ReportsLv.Font = null;
-            this.ReportsLv.FullRowSelect = true;
-            this.ReportsLv.HeaderStyle = System.Windows.Forms.ColumnHeaderStyle.Nonclickable;
-            this.ReportsLv.Name = "ReportsLv";
-            this.ReportsLv.UseCompatibleStateImageBehavior = false;
-            this.ReportsLv.View = System.Windows.Forms.View.Details;
-            this.ReportsLv.ItemActivate += new System.EventHandler(this.ReportsLv_ItemActivate);
-            // 
-            // ReportsLvTimestampColumn
-            // 
-            resources.ApplyResources(this.ReportsLvTimestampColumn, "ReportsLvTimestampColumn");
-            // 
-            // ReportsLvErrorColumn
-            // 
-            resources.ApplyResources(this.ReportsLvErrorColumn, "ReportsLvErrorColumn");
-            // 
-            // BlackBoxMainForm
-            // 
-            this.AccessibleDescription = null;
-            this.AccessibleName = null;
-            resources.ApplyResources(this, "$this");
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
-            this.BackgroundImage = null;
-            this.Controls.Add(this.ReportsLv);
-            this.Controls.Add(this.BlackBoxPic);
-            this.Controls.Add(this.PostponeBtn);
-            this.Controls.Add(this.SubmitBtn);
-            this.Controls.Add(this.MainLbl);
-            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
-            this.MaximizeBox = false;
-            this.MinimizeBox = false;
-            this.Name = "BlackBoxMainForm";
-            this.ShowInTaskbar = false;
-            this.TopMost = true;
-            ((System.ComponentModel.ISupportInitialize)(this.BlackBoxPic)).EndInit();
-            this.ResumeLayout(false);
+			this.ReportsLv.FullRowSelect = true;
+			this.ReportsLv.HeaderStyle = System.Windows.Forms.ColumnHeaderStyle.Nonclickable;
+			this.ReportsLv.Name = "ReportsLv";
+			this.ReportsLv.UseCompatibleStateImageBehavior = false;
+			this.ReportsLv.View = System.Windows.Forms.View.Details;
+			this.ReportsLv.ItemActivate += new System.EventHandler(this.ReportsLv_ItemActivate);
+			// 
+			// ReportsLvTimestampColumn
+			// 
+			resources.ApplyResources(this.ReportsLvTimestampColumn, "ReportsLvTimestampColumn");
+			// 
+			// ReportsLvErrorColumn
+			// 
+			resources.ApplyResources(this.ReportsLvErrorColumn, "ReportsLvErrorColumn");
+			// 
+			// BlackBoxMainForm
+			// 
+			resources.ApplyResources(this, "$this");
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+			this.Controls.Add(this.ReportsLv);
+			this.Controls.Add(this.BlackBoxPic);
+			this.Controls.Add(this.PostponeBtn);
+			this.Controls.Add(this.SubmitBtn);
+			this.Controls.Add(this.MainLbl);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+			this.MaximizeBox = false;
+			this.MinimizeBox = false;
+			this.Name = "BlackBoxMainForm";
+			this.ShowInTaskbar = false;
+			this.TopMost = true;
+			((System.ComponentModel.ISupportInitialize)(this.BlackBoxPic)).EndInit();
+			this.ResumeLayout(false);
 
 		}
 
