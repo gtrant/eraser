b13f31f37b862c0699eb1c27b6548b0dd08b5c52
lowjoel <lowjoel@5a19fcc5-093c-0410-9d93-cd3e460611b7>
2010-04-28 06:37:21 +0000

    Implemented the IPlugin interface and output to the Plugins directory so it can be loaded by Eraser.

--- eraser/Eraser.BlackBox/Plugin.cs
+++ eraser/Eraser.BlackBox/Plugin.cs
@@ -1,17 +1,67 @@
-﻿using System;
+﻿/* 
+ * $Id$
+ * Copyright 2008-2010 The Eraser Project
+ * Original Author: Joel Low <lowjoel@users.sourceforge.net>
+ * Modified By:
+ * 
+ * This file is part of Eraser.
+ * 
+ * Eraser is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later
+ * version.
+ * 
+ * Eraser is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ * 
+ * A copy of the GNU General Public License can be found at
+ * <http://www.gnu.org/licenses/>.
+ */
+
+using System;
 using System.Collections.Generic;
 using System.Text;
-
 using System.Windows.Forms;
+
+using Eraser.Manager.Plugin;
 
 namespace Eraser.BlackBox
 {
-	class Plugin
+	public sealed class Plugin : IPlugin
 	{
-		void Init()
+		public void Initialize(Host host)
 		{
 			//Initialise our crash handler
 			BlackBox blackBox = BlackBox.Get();
+
+			//Hook the Application's idle loop to display the form
+			Application.Idle += OnGUIIdle;
+		}
+
+		public void Dispose()
+		{
+			GC.SuppressFinalize(this);
+		}
+
+		public string Name
+		{
+			get { throw new NotImplementedException(); }
+		}
+
+		public string Author
+		{
+			get { throw new NotImplementedException(); }
+		}
+
+		public bool Configurable
+		{
+			get { throw new NotImplementedException(); }
+		}
+
+		public void DisplaySettings(Control parent)
+		{
+			throw new NotImplementedException();
 		}
 
 		public static void OnGUIIdle(object sender, EventArgs e)
