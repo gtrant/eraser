1283ba743ae8b270c329700c62c2942dd7c3e668
lowjoel <lowjoel@5a19fcc5-093c-0410-9d93-cd3e460611b7>
2010-05-03 12:18:43 +0000

    Create placeholders to configure the different erasure target types.

---
+++ eraser/Eraser.Manager/UnusedSpaceErasureTargetSettings.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Eraser.Manager
+{
+	public partial class UnusedSpaceErasureTargetSettings : UserControl
+	{
+		public UnusedSpaceErasureTargetSettings()
+		{
+			InitializeComponent();
+		}
+	}
+}
