a1255e4ec7b4ddeec3133098489f55a83e2b4110
lowjoel <lowjoel@5a19fcc5-093c-0410-9d93-cd3e460611b7>
2010-05-04 07:02:41 +0000

    Implemented drag & drop from shell virtual folders (mainly the Recycle Bin)

--- eraser/Eraser.Util/Shell.cs
+++ eraser/Eraser.Util/Shell.cs
@@ -21,6 +21,7 @@
 
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 using System.Runtime.InteropServices;
@@ -85,6 +86,241 @@ namespace Eraser.Util
 			NativeMethods.LocalFree(argv);
 
 			return result;
+		}
+
+		/// <summary>
+		/// A List of known folder IDs in the shell namespace.
+		/// </summary>
+		public static class KnownFolderIDs
+		{
+			/// <summary>
+			/// The Known Folder ID of the Recycle Bin
+			/// </summary>
+			public static readonly Guid RecycleBin = 
+				new Guid(0xB7534046, 0x3ECB, 0x4C18, 0xBE, 0x4E, 0x64, 0xCD, 0x4C, 0xB7, 0xD6, 0xAC);
+
+			/// <summary>
+			/// Gets the PIDL for the given Known folder ID.
+			/// </summary>
+			/// <param name="folderId">The known folder ID to query.</param>
+			/// <returns>The PIDL for the given folder.</returns>
+			public static ShellItemIDList GetShellItemIdList(Guid folderId)
+			{
+				Guid guid = folderId;
+				IntPtr pidl = IntPtr.Zero;
+				NativeMethods.SHGetKnownFolderIDList(ref guid, 0, IntPtr.Zero, out pidl);
+
+				try
+				{
+					return new ShellItemIDList(pidl);
+				}
+				finally
+				{
+					NativeMethods.ILFree(pidl);
+				}
+			}
+		}
+	}
+
+	/// <summary>
+	/// Retrieves the path of a known folder as an ITEMIDLIST structure.
+	/// </summary>
+	public class ShellCIDA
+	{
+		/// <summary>
+		/// Parses the given buffer for CIDA elements
+		/// </summary>
+		/// <param name="buffer"></param>
+		public ShellCIDA(byte[] buffer)
+		{
+			int offset = 0;
+			cidl = BitConverter.ToUInt32(buffer, offset);
+			aoffset = new ShellItemIDList[cidl + 1];
+
+			for (int i = 0; i < aoffset.Length; ++i)
+			{
+				int pidlOffset = BitConverter.ToInt32(buffer, offset += sizeof(int));
+
+				//Read the size of the IDL
+				aoffset[i] = new ShellItemIDList(buffer.Skip(pidlOffset).ToArray());
+			}
+		}
+
+		/// <summary>
+		/// The number of PIDLs that are being transferred, not including the parent folder.
+		/// </summary>
+		public uint cidl
+		{
+			get;
+			private set;
+		}
+
+		/// <summary>
+		/// The first element of aoffset contains the fully-qualified PIDL of a parent folder.
+		/// If this PIDL is empty, the parent folder is the desktop. Each of the remaining
+		/// elements of the array contains an offset to one of the PIDLs to be transferred.
+		/// All of these PIDLs are relative to the PIDL of the parent folder.
+		/// </summary>
+		public ShellItemIDList[] aoffset
+		{
+			get;
+			private set;
+		}
+	}
+
+	/// <summary>
+	/// Contains a list of item identifiers.
+	/// </summary>
+	public class ShellItemIDList
+	{
+		public ShellItemIDList(byte[] buffer)
+		{
+			mkid = new ShellItemID(buffer);
+		}
+
+		public ShellItemIDList(IntPtr buffer)
+		{
+			mkid = new ShellItemID(buffer);
+		}
+
+		public ShellItemID mkid
+		{
+			get;
+			private set;
+		}
+
+		/// <summary>
+		/// The physical path to the object referenced by this IDL.
+		/// </summary>
+		/// <remarks>If this IDL references a virtual object, this will return
+		/// null.</remarks>
+		public string Path
+		{
+			get
+			{
+				IntPtr mkid = this.mkid.ToSHITEMID();
+				try
+				{
+					StringBuilder result = new StringBuilder(NativeMethods.MaxPath);
+					if (NativeMethods.SHGetPathFromIDList(mkid, result))
+						return result.ToString();
+				}
+				finally
+				{
+					Marshal.FreeHGlobal(mkid);
+				}
+
+				return null;
+			}
+		}
+
+		/// <summary>
+		/// The GUID of the virtual folder referenced by this IDL.
+		/// </summary>
+		/// <remarks>If this IDL references a physical object, this will return
+		/// <see cref="Guid.Empty"/></remarks>
+		public Guid Guid
+		{
+			get
+			{
+				Guid[] guids = new Guid[] {
+					Shell.KnownFolderIDs.RecycleBin
+				};
+
+				foreach (Guid guid in guids)
+				{
+					if (Shell.KnownFolderIDs.GetShellItemIdList(guid) == this)
+						return guid;
+				}
+
+				return Guid.Empty;
+			}
+		}
+
+		public static bool operator==(ShellItemIDList lhs, ShellItemIDList rhs)
+		{
+			return lhs.mkid == rhs.mkid;
+		}
+
+		public static bool operator!=(ShellItemIDList lhs, ShellItemIDList rhs)
+		{
+			return lhs.mkid != rhs.mkid;
+		}
+
+		public override bool Equals(object obj)
+		{
+			if (obj is ShellItemIDList)
+				return this == (ShellItemIDList)obj;
+			return this.Equals(obj);
+		}
+
+		public override int GetHashCode()
+		{
+			return base.GetHashCode();
+		}
+	}
+
+	/// <summary>
+	/// Defines an item identifier. (native type: SHITEMID)
+	/// </summary>
+	public class ShellItemID
+	{
+		public ShellItemID(byte[] buffer)
+		{
+			short cb = BitConverter.ToInt16(buffer, 0);
+			abID = new byte[cb];
+			if (cb > 0)
+				Buffer.BlockCopy(buffer, sizeof(short), abID, 0, cb - sizeof(short));
+		}
+
+		public ShellItemID(IntPtr buffer)
+		{
+			short cb = Marshal.ReadInt16(buffer);
+			abID = new byte[cb];
+			if (cb > 0)
+				Marshal.Copy(new IntPtr(buffer.ToInt64() + sizeof(short)), abID, 0, cb - sizeof(short));
+		}
+
+		byte[] abID;
+
+		/// <summary>
+		/// Converts this ShellItemID to the native SHITEMID.
+		/// </summary>
+		/// <returns>A Pointer to an unmanaged block of memory which should be
+		/// freed by Marshal.FreeHGlobal upon completion.</returns>
+		internal IntPtr ToSHITEMID()
+		{
+			//Allocate the buffer
+			IntPtr result = Marshal.AllocHGlobal(abID.Length + (abID.Length == 0 ? 0 : sizeof(short)));
+
+			//Write the size of the identifier
+			Marshal.WriteInt16(result, (short)abID.Length);
+
+			//Then copy the block of memory
+			Marshal.Copy(abID, 0, new IntPtr(result.ToInt64() + 2), abID.Length);
+			return result;
+		}
+
+		public static bool operator==(ShellItemID lhs, ShellItemID rhs)
+		{
+			return lhs.abID.SequenceEqual(rhs.abID);
+		}
+
+		public static bool operator!=(ShellItemID lhs, ShellItemID rhs)
+		{
+			return !lhs.abID.SequenceEqual(rhs.abID);
+		}
+
+		public override bool Equals(object obj)
+		{
+			if (obj is ShellItemID)
+				return this == (ShellItemID)obj;
+			return this.Equals(obj);
+		}
+
+		public override int GetHashCode()
+		{
+			return base.GetHashCode();
 		}
 	}
 }
