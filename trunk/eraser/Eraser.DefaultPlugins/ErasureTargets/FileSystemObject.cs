2c8f1791b39b1efd7f2c108d7b626440ebbb84e5
lowjoel <lowjoel@5a19fcc5-093c-0410-9d93-cd3e460611b7>
2010-05-04 00:28:39 +0000

    Split the huge File.cs into its constituent classes in separate files.

---
+++ trunk/eraser/Eraser.DefaultPlugins/ErasureTargets/FileSystemObject.cs
@@ -0,0 +1,302 @@
+﻿/* 
+ * $Id$
+ * Copyright 2008-2010 The Eraser Project
+ * Original Author: Joel Low <lowjoel@users.sourceforge.net>
+ * Modified By:
+ * 
+ * This file is part of Eraser.
+ * 
+ * Eraser is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later
+ * version.
+ * 
+ * Eraser is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ * 
+ * A copy of the GNU General Public License can be found at
+ * <http://www.gnu.org/licenses/>.
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using System.Runtime.Serialization;
+using System.Runtime.InteropServices;
+using System.Security.Permissions;
+using System.IO;
+
+using Eraser.Manager;
+using Eraser.Util;
+using Eraser.Util.ExtensionMethods;
+
+namespace Eraser.DefaultPlugins
+{
+	/// <summary>
+	/// Class representing a tangible object (file/folder) to be erased.
+	/// </summary>
+	[Serializable]
+	public abstract class FileSystemObjectTarget : ErasureTarget
+	{
+		#region Serialization code
+		protected FileSystemObjectTarget(SerializationInfo info, StreamingContext context)
+			: base(info, context)
+		{
+			Path = (string)info.GetValue("Path", typeof(string));
+		}
+
+		[SecurityPermission(SecurityAction.Demand, SerializationFormatter = true)]
+		public override void GetObjectData(SerializationInfo info, StreamingContext context)
+		{
+			base.GetObjectData(info, context);
+			info.AddValue("Path", Path);
+		}
+		#endregion
+
+		/// <summary>
+		/// Constructor.
+		/// </summary>
+		protected FileSystemObjectTarget()
+			: base()
+		{
+		}
+
+		protected new SteppedProgressManager Progress
+		{
+			get
+			{
+				return (SteppedProgressManager)base.Progress;
+			}
+			set
+			{
+				base.Progress = value;
+			}
+		}
+
+		/// <summary>
+		/// Retrieves the list of files/folders to erase as a list.
+		/// </summary>
+		/// <param name="totalSize">Returns the total size in bytes of the
+		/// items.</param>
+		/// <returns>A list containing the paths to all the files to be erased.</returns>
+		internal abstract List<string> GetPaths(out long totalSize);
+
+		/// <summary>
+		/// Adds ADSes of the given file to the list.
+		/// </summary>
+		/// <param name="list">The list to add the ADS paths to.</param>
+		/// <param name="file">The file to look for ADSes</param>
+		protected void GetPathADSes(ICollection<string> list, out long totalSize, string file)
+		{
+			totalSize = 0;
+
+			try
+			{
+				//Get the ADS names
+				IList<string> adses = new FileInfo(file).GetADSes();
+
+				//Then prepend the path.
+				foreach (string adsName in adses)
+				{
+					string adsPath = file + ':' + adsName;
+					list.Add(adsPath);
+					StreamInfo info = new StreamInfo(adsPath);
+					totalSize += info.Length;
+				}
+			}
+			catch (FileNotFoundException)
+			{
+			}
+			catch (SharingViolationException)
+			{
+				//The system cannot open the file, try to force the file handle to close.
+				if (!ManagerLibrary.Settings.ForceUnlockLockedFiles)
+					throw;
+
+				StringBuilder processStr = new StringBuilder();
+				foreach (OpenHandle handle in OpenHandle.Close(file))
+				{
+					try
+					{
+						processStr.AppendFormat(
+							System.Globalization.CultureInfo.InvariantCulture,
+							"{0}, ", (System.Diagnostics.Process.GetProcessById(handle.ProcessId)).MainModule.FileName);
+					}
+					catch (System.ComponentModel.Win32Exception)
+					{
+						processStr.AppendFormat(
+							System.Globalization.CultureInfo.InvariantCulture,
+							"Process ID {0}, ", handle.ProcessId);
+					}
+				}
+
+				if (processStr.Length == 0)
+				{
+					GetPathADSes(list, out totalSize, file);
+					return;
+				}
+				else
+					throw;
+			}
+			catch (UnauthorizedAccessException e)
+			{
+				//The system cannot read the file, assume no ADSes for lack of
+				//more information.
+				Logger.Log(e.Message, LogLevel.Error);
+			}
+		}
+
+		/// <summary>
+		/// The path to the file or folder referred to by this object.
+		/// </summary>
+		public string Path { get; set; }
+
+		public sealed override ErasureMethod EffectiveMethod
+		{
+			get
+			{
+				if (Method != ErasureMethodRegistrar.Default)
+					return base.EffectiveMethod;
+
+				return ManagerLibrary.Instance.ErasureMethodRegistrar[
+					ManagerLibrary.Settings.DefaultFileErasureMethod];
+			}
+		}
+
+		public override string UIText
+		{
+			get
+			{
+				string fileName = System.IO.Path.GetFileName(Path);
+				string directoryName = System.IO.Path.GetDirectoryName(Path);
+				return string.IsNullOrEmpty(fileName) ?
+						(string.IsNullOrEmpty(directoryName) ? Path : directoryName)
+					: fileName;
+			}
+		}
+
+		public override void Execute()
+		{
+			//Retrieve the list of files to erase.
+			long dataTotal = 0;
+			List<string> paths = GetPaths(out dataTotal);
+
+			//Get the erasure method if the user specified he wants the default.
+			ErasureMethod method = EffectiveMethod;
+			dataTotal = method.CalculateEraseDataSize(paths, dataTotal);
+
+			//Set the event's current target status.
+			SteppedProgressManager progress = new SteppedProgressManager();
+			Progress = progress;
+			Task.Progress.Steps.Add(new SteppedProgressManagerStep(progress, 1.0f / Task.Targets.Count));
+
+			//Iterate over every path, and erase the path.
+			for (int i = 0; i < paths.Count; ++i)
+			{
+				//Check that the file exists - we do not want to bother erasing nonexistant files
+				StreamInfo info = new StreamInfo(paths[i]);
+				if (!info.Exists)
+				{
+					Logger.Log(S._("The file {0} was not erased as the file does not exist.",
+						paths[i]), LogLevel.Notice);
+					continue;
+				}
+
+				//Get the filesystem provider to handle the secure file erasures
+				FileSystem fsManager = ManagerLibrary.Instance.FileSystemRegistrar[
+					VolumeInfo.FromMountPoint(info.DirectoryName)];
+
+				bool isReadOnly = false;
+
+				try
+				{
+					//Update the task progress
+					ProgressManager step = new ProgressManager();
+					progress.Steps.Add(new SteppedProgressManagerStep(step,
+						info.Length / (float)dataTotal, S._("Erasing files...")));
+					OnProgressChanged(this, new ProgressChangedEventArgs(step,
+						new TaskProgressChangedEventArgs(paths[i], 0, method.Passes)));
+
+					//Remove the read-only flag, if it is set.
+					if (isReadOnly = info.IsReadOnly)
+						info.IsReadOnly = false;
+
+					//Make sure the file does not have any attributes which may affect
+					//the erasure process
+					if ((info.Attributes & FileAttributes.Compressed) != 0 ||
+						(info.Attributes & FileAttributes.Encrypted) != 0 ||
+						(info.Attributes & FileAttributes.SparseFile) != 0)
+					{
+						//Log the error
+						Logger.Log(S._("The file {0} could not be erased because the file was " +
+							"either compressed, encrypted or a sparse file.", info.FullName),
+							LogLevel.Error);
+						continue;
+					}
+
+					fsManager.EraseFileSystemObject(info, method,
+						delegate(long lastWritten, long totalData, int currentPass)
+						{
+							if (Task.Canceled)
+								throw new OperationCanceledException(S._("The task was cancelled."));
+
+							step.Total = totalData;
+							step.Completed += lastWritten;
+							OnProgressChanged(this, new ProgressChangedEventArgs(step,
+								new TaskProgressChangedEventArgs(info.FullName, currentPass, method.Passes)));
+						});
+
+					//Remove the file.
+					FileInfo fileInfo = info.File;
+					if (fileInfo != null)
+						fsManager.DeleteFile(fileInfo);
+					step.MarkComplete();
+				}
+				catch (UnauthorizedAccessException)
+				{
+					Logger.Log(S._("The file {0} could not be erased because the file's " +
+						"permissions prevent access to the file.", info.FullName), LogLevel.Error);
+				}
+				catch (SharingViolationException)
+				{
+					if (!ManagerLibrary.Settings.ForceUnlockLockedFiles)
+						throw;
+
+					StringBuilder processStr = new StringBuilder();
+					foreach (OpenHandle handle in OpenHandle.Close(info.FullName))
+					{
+						try
+						{
+							processStr.AppendFormat(
+								System.Globalization.CultureInfo.InvariantCulture,
+								"{0}, ", System.Diagnostics.Process.GetProcessById(handle.ProcessId).MainModule.FileName);
+						}
+						catch (System.ComponentModel.Win32Exception)
+						{
+							processStr.AppendFormat(
+								System.Globalization.CultureInfo.InvariantCulture,
+								"Process ID {0}, ", handle.ProcessId);
+						}
+					}
+
+					if (processStr.Length != 0)
+						Logger.Log(S._("Could not force closure of file \"{0}\" {1}",
+								paths[i], S._("(locked by {0})",
+									processStr.ToString().Remove(processStr.Length - 2)).Trim()),
+							LogLevel.Error);
+				}
+				finally
+				{
+					//Re-set the read-only flag if the file exists (i.e. there was an error)
+					if (isReadOnly && info.Exists && !info.IsReadOnly)
+						info.IsReadOnly = isReadOnly;
+				}
+			}
+
+			Progress = null;
+		}
+	}
+}
---
+++ trunk/eraser/Eraser.DefaultPlugins/ErasureTargets/RecycleBin.cs
@@ -0,0 +1,147 @@
+﻿/* 
+ * $Id$
+ * Copyright 2008-2010 The Eraser Project
+ * Original Author: Joel Low <lowjoel@users.sourceforge.net>
+ * Modified By:
+ * 
+ * This file is part of Eraser.
+ * 
+ * Eraser is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later
+ * version.
+ * 
+ * Eraser is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ * 
+ * A copy of the GNU General Public License can be found at
+ * <http://www.gnu.org/licenses/>.
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using System.Runtime.Serialization;
+using System.Runtime.InteropServices;
+using System.IO;
+
+using Eraser.Manager;
+using Eraser.Util;
+
+namespace Eraser.DefaultPlugins
+{
+	[Serializable]
+	[Guid("A1FA7354-0258-4903-88E9-0D31FC5F8D51")]
+	public class RecycleBinTarget : FileSystemObjectTarget
+	{
+		#region Serialization code
+		protected RecycleBinTarget(SerializationInfo info, StreamingContext context)
+			: base(info, context)
+		{
+		}
+		#endregion
+
+		public RecycleBinTarget()
+		{
+		}
+
+		public override Guid Guid
+		{
+			get { return GetType().GUID; }
+		}
+
+		public override IErasureTargetConfigurer Configurer
+		{
+			get { return null; }
+		}
+
+		internal override List<string> GetPaths(out long totalSize)
+		{
+			totalSize = 0;
+			List<string> result = new List<string>();
+			string[] rootDirectory = new string[] {
+					"$RECYCLE.BIN",
+					"RECYCLER"
+				};
+
+			foreach (DriveInfo drive in DriveInfo.GetDrives())
+			{
+				foreach (string rootDir in rootDirectory)
+				{
+					DirectoryInfo dir = new DirectoryInfo(
+						System.IO.Path.Combine(
+							System.IO.Path.Combine(drive.Name, rootDir),
+							System.Security.Principal.WindowsIdentity.GetCurrent().
+								User.ToString()));
+					if (!dir.Exists)
+						continue;
+
+					GetRecyclerFiles(dir, result, ref totalSize);
+				}
+			}
+
+			return result;
+		}
+
+		/// <summary>
+		/// Retrieves all files within this folder, without exclusions.
+		/// </summary>
+		/// <param name="info">The DirectoryInfo object representing the folder to traverse.</param>
+		/// <param name="paths">The list of files to store path information in.</param>
+		/// <param name="totalSize">Receives the total size of the files.</param>
+		private void GetRecyclerFiles(DirectoryInfo info, List<string> paths,
+			ref long totalSize)
+		{
+			try
+			{
+				foreach (FileInfo fileInfo in info.GetFiles())
+				{
+					if (!fileInfo.Exists || (fileInfo.Attributes & FileAttributes.ReparsePoint) != 0)
+						continue;
+
+					long adsSize = 0;
+					GetPathADSes(paths, out adsSize, fileInfo.FullName);
+					totalSize += adsSize;
+					totalSize += fileInfo.Length;
+					paths.Add(fileInfo.FullName);
+				}
+
+				foreach (DirectoryInfo directoryInfo in info.GetDirectories())
+					if ((directoryInfo.Attributes & FileAttributes.ReparsePoint) == 0)
+						GetRecyclerFiles(directoryInfo, paths, ref totalSize);
+			}
+			catch (UnauthorizedAccessException e)
+			{
+				Logger.Log(e.Message, LogLevel.Error);
+			}
+		}
+
+		/// <summary>
+		/// Retrieves the text to display representing this task.
+		/// </summary>
+		public override string UIText
+		{
+			get
+			{
+				return S._("Recycle Bin");
+			}
+		}
+
+		public override void Execute()
+		{
+			base.Execute();
+
+			ProgressManager step = new ProgressManager();
+			Progress.Steps.Add(new SteppedProgressManagerStep(step,
+				0.0f, S._("Emptying recycle bin...")));
+			OnProgressChanged(this, new ProgressChangedEventArgs(step,
+				new TaskProgressChangedEventArgs(string.Empty, 0, 0)));
+
+			RecycleBin.Empty(EmptyRecycleBinOptions.NoConfirmation |
+				EmptyRecycleBinOptions.NoProgressUI | EmptyRecycleBinOptions.NoSound);
+		}
+	}
+}
