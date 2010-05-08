b9fd80c0492a44a234bcfd37e3170772f9e10b0b
lowjoel <lowjoel@5a19fcc5-093c-0410-9d93-cd3e460611b7>
2010-05-08 00:49:01 +0000

    Implement a SplitButton class for use with the SecureMoveErasureTargetConfigurer class.

---
+++ eraser/Eraser.Util/ExtensionClasses/SplitButton.cs
@@ -0,0 +1,111 @@
+﻿/* 
+ * $Id$
+ * Copyright 2008-2010 The Eraser Project
+ * Original Author: Joel Low <lowjoel@users.sourceforge.net>
+ * Modified By:
+ * 
+ * This file is part of Eraser.
+ * 
+ * Eraser is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later
+ * version.
+ * 
+ * Eraser is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ * 
+ * A copy of the GNU General Public License can be found at
+ * <http://www.gnu.org/licenses/>.
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Eraser.Util;
+using System.Drawing;
+
+namespace System.Windows.Forms
+{
+	public class SplitButton : Button
+	{
+		/// <summary>
+		/// Constructor.
+		/// </summary>
+		public SplitButton()
+		{
+			FlatStyle = FlatStyle.System;
+		}
+
+		protected override CreateParams CreateParams
+		{
+			get
+			{
+				CreateParams createParams = base.CreateParams;
+				createParams.Style |= NativeMethods.BS_SPLITBUTTON;
+				return createParams;
+			}
+		}
+
+		protected override void WndProc(ref Message m)
+		{
+			switch ((uint)m.Msg)
+			{
+				case 0x2000 | NativeMethods.WM_NOTIFY:
+					//Reflected WM_NOTIFY from parent. Check that the handle is ours
+					if (m.HWnd == Handle)
+					{
+						//Then check the code of the message
+						NativeMethods.NMHDR nmHdr = (NativeMethods.NMHDR)
+							m.GetLParam(typeof(NativeMethods.NMHDR));
+
+						//Handle only BCN_DROPDOWN messages
+						if (nmHdr.code == NativeMethods.BCN_DROPDOWN)
+						{
+							//The dropdown portion of the button is being pressed, show the menu
+							if (ContextMenuStrip != null)
+							{
+								Point point = new Point(Width - ContextMenuStrip.Width, Height);
+								ContextMenuStrip.Show(this, point, ToolStripDropDownDirection.BelowRight);
+								ContextMenuStrip.Closed += OnMenuClosed;
+							}
+						}
+					}
+					break;
+
+				case NativeMethods.WM_PAINT:
+					//Paint the control to have the dropdown portion as pressed when the
+					//menu is shown.
+					DropDownState = ContextMenuStrip != null && ContextMenuStrip.Visible;
+					break;
+
+				case NativeMethods.WM_CONTEXTMENU:
+					//Swallow all context menu clicks on Vista and later -- otherwise
+					//we will also show the context menu when the user right-clicks
+					//the button.
+					if (Environment.OSVersion.Version.Major >= 6)
+						return;
+					break;
+			}
+
+			base.WndProc(ref m);
+		}
+
+		private bool DropDownState
+		{
+			set
+			{
+				NativeMethods.SendMessage(this.Handle, NativeMethods.BCM_SETDROPDOWNSTATE,
+					new UIntPtr(value ? 1U : 0), IntPtr.Zero);
+			}
+		}
+
+		private void OnMenuClosed(object sender, EventArgs e)
+		{
+			DropDownState = false;
+			ContextMenuStrip.Closed -= OnMenuClosed;
+		}
+	}
+}
---
+++ eraser/Eraser.Util/NativeMethods/WinUser.cs
@@ -0,0 +1,47 @@
+﻿/* 
+ * $Id$
+ * Copyright 2008-2010 The Eraser Project
+ * Original Author: Joel Low <lowjoel@users.sourceforge.net>
+ * Modified By:
+ * 
+ * This file is part of Eraser.
+ * 
+ * Eraser is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later
+ * version.
+ * 
+ * Eraser is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ * 
+ * A copy of the GNU General Public License can be found at
+ * <http://www.gnu.org/licenses/>.
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Eraser.Util
+{
+	internal static partial class NativeMethods
+	{
+		public const int BS_SPLITBUTTON = 0x0000000C;
+		public const uint BCM_FIRST = 0x1600;
+		public const uint BCM_SETDROPDOWNSTATE = BCM_FIRST + 6;
+		public const uint BCN_FIRST = unchecked(0U - 1250U);
+		public const uint BCN_DROPDOWN = BCN_FIRST + 0x0002;
+		public const uint WM_NOTIFY = 0x004E;
+		public const uint WM_PAINT = 0x000F;
+		public const uint WM_CONTEXTMENU = 0x007B;
+
+		public struct NMHDR
+		{
+			public IntPtr hwndFrom;
+			public uint idFrom;
+			public uint code;
+		}
+	}
+}
