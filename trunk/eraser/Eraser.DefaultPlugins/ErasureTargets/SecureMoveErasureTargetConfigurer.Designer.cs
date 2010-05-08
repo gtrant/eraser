2ce6bd834c981876d17f8ba4635bdc1b597fb165
lowjoel <lowjoel@5a19fcc5-093c-0410-9d93-cd3e460611b7>
2010-05-08 05:00:27 +0000

    Don't use the split button to pick the file or folder to move to; ask the user what he wants moved, then present the appropriate dialogs. Addresses #60: Eraser Secure Cut.

--- trunk/eraser/Eraser.DefaultPlugins/ErasureTargets/SecureMoveErasureTargetConfigurer.Designer.cs
+++ trunk/eraser/Eraser.DefaultPlugins/ErasureTargets/SecureMoveErasureTargetConfigurer.Designer.cs
@@ -49,25 +49,23 @@ namespace Eraser.DefaultPlugins
 		/// </summary>
 		private void InitializeComponent()
 		{
-			this.components = new System.ComponentModel.Container();
-			this.fileDialog = new System.Windows.Forms.OpenFileDialog();
+			this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
 			this.folderDialog = new System.Windows.Forms.FolderBrowserDialog();
 			this.fromLbl = new System.Windows.Forms.Label();
 			this.fromTxt = new System.Windows.Forms.TextBox();
 			this.toLbl = new System.Windows.Forms.Label();
 			this.toTxt = new System.Windows.Forms.TextBox();
-			this.fromBrowseMenu = new System.Windows.Forms.ContextMenuStrip(this.components);
-			this.fromSelectFile = new System.Windows.Forms.ToolStripMenuItem();
-			this.fromSelectFolder = new System.Windows.Forms.ToolStripMenuItem();
 			this.toSelectButton = new System.Windows.Forms.Button();
-			this.fromSelectButton = new System.Windows.Forms.SplitButton();
-			this.fromBrowseMenu.SuspendLayout();
+			this.moveFileRadio = new System.Windows.Forms.RadioButton();
+			this.moveFolderRadio = new System.Windows.Forms.RadioButton();
+			this.fromSelectBtn = new System.Windows.Forms.Button();
+			this.saveFileDialog = new System.Windows.Forms.SaveFileDialog();
 			this.SuspendLayout();
 			// 
 			// fromLbl
 			// 
 			this.fromLbl.AutoSize = true;
-			this.fromLbl.Location = new System.Drawing.Point(-3, 3);
+			this.fromLbl.Location = new System.Drawing.Point(-3, 28);
 			this.fromLbl.Name = "fromLbl";
 			this.fromLbl.Size = new System.Drawing.Size(38, 15);
 			this.fromLbl.TabIndex = 0;
@@ -77,15 +75,15 @@ namespace Eraser.DefaultPlugins
 			// 
 			this.fromTxt.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
 						| System.Windows.Forms.AnchorStyles.Right)));
-			this.fromTxt.Location = new System.Drawing.Point(41, 0);
+			this.fromTxt.Location = new System.Drawing.Point(41, 25);
 			this.fromTxt.Name = "fromTxt";
 			this.fromTxt.Size = new System.Drawing.Size(397, 23);
-			this.fromTxt.TabIndex = 1;
+			this.fromTxt.TabIndex = 3;
 			// 
 			// toLbl
 			// 
 			this.toLbl.AutoSize = true;
-			this.toLbl.Location = new System.Drawing.Point(-3, 33);
+			this.toLbl.Location = new System.Drawing.Point(-3, 57);
 			this.toLbl.Name = "toLbl";
 			this.toLbl.Size = new System.Drawing.Size(24, 15);
 			this.toLbl.TabIndex = 4;
@@ -95,71 +93,71 @@ namespace Eraser.DefaultPlugins
 			// 
 			this.toTxt.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
 						| System.Windows.Forms.AnchorStyles.Right)));
-			this.toTxt.Location = new System.Drawing.Point(41, 30);
+			this.toTxt.Location = new System.Drawing.Point(41, 54);
 			this.toTxt.Name = "toTxt";
 			this.toTxt.Size = new System.Drawing.Size(397, 23);
 			this.toTxt.TabIndex = 5;
 			// 
-			// fromBrowseMenu
-			// 
-			this.fromBrowseMenu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.fromSelectFile,
-            this.fromSelectFolder});
-			this.fromBrowseMenu.Name = "contextMenuStrip1";
-			this.fromBrowseMenu.Size = new System.Drawing.Size(176, 48);
-			// 
-			// fromSelectFile
-			// 
-			this.fromSelectFile.Name = "fromSelectFile";
-			this.fromSelectFile.Size = new System.Drawing.Size(175, 22);
-			this.fromSelectFile.Text = "Browse for File...";
-			this.fromSelectFile.Click += new System.EventHandler(this.fromSelectButton_Click);
-			// 
-			// fromSelectFolder
-			// 
-			this.fromSelectFolder.Name = "fromSelectFolder";
-			this.fromSelectFolder.Size = new System.Drawing.Size(175, 22);
-			this.fromSelectFolder.Text = "Browse for Folder...";
-			this.fromSelectFolder.Click += new System.EventHandler(this.fromSelectFolder_Click);
-			// 
 			// toSelectButton
 			// 
 			this.toSelectButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-			this.toSelectButton.Location = new System.Drawing.Point(444, 29);
+			this.toSelectButton.Location = new System.Drawing.Point(444, 53);
 			this.toSelectButton.Name = "toSelectButton";
 			this.toSelectButton.Size = new System.Drawing.Size(75, 23);
-			this.toSelectButton.TabIndex = 10;
+			this.toSelectButton.TabIndex = 6;
 			this.toSelectButton.Text = "Browse";
 			this.toSelectButton.UseVisualStyleBackColor = true;
 			this.toSelectButton.Click += new System.EventHandler(this.toSelectButton_Click);
 			// 
-			// fromSelectButton
+			// moveFileRadio
 			// 
-			this.fromSelectButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-			this.fromSelectButton.ContextMenuStrip = this.fromBrowseMenu;
-			this.fromSelectButton.FlatStyle = System.Windows.Forms.FlatStyle.System;
-			this.fromSelectButton.Location = new System.Drawing.Point(444, -1);
-			this.fromSelectButton.Name = "fromSelectButton";
-			this.fromSelectButton.Size = new System.Drawing.Size(75, 23);
-			this.fromSelectButton.TabIndex = 7;
-			this.fromSelectButton.Text = "Browse";
-			this.fromSelectButton.UseVisualStyleBackColor = true;
-			this.fromSelectButton.Click += new System.EventHandler(this.fromSelectButton_Click);
+			this.moveFileRadio.AutoSize = true;
+			this.moveFileRadio.Checked = true;
+			this.moveFileRadio.Location = new System.Drawing.Point(0, 0);
+			this.moveFileRadio.Name = "moveFileRadio";
+			this.moveFileRadio.Size = new System.Drawing.Size(85, 19);
+			this.moveFileRadio.TabIndex = 1;
+			this.moveFileRadio.TabStop = true;
+			this.moveFileRadio.Text = "Move a File";
+			this.moveFileRadio.UseVisualStyleBackColor = true;
+			// 
+			// moveFolderRadio
+			// 
+			this.moveFolderRadio.AutoSize = true;
+			this.moveFolderRadio.Location = new System.Drawing.Point(91, 0);
+			this.moveFolderRadio.Name = "moveFolderRadio";
+			this.moveFolderRadio.Size = new System.Drawing.Size(100, 19);
+			this.moveFolderRadio.TabIndex = 2;
+			this.moveFolderRadio.TabStop = true;
+			this.moveFolderRadio.Text = "Move a Folder";
+			this.moveFolderRadio.UseVisualStyleBackColor = true;
+			// 
+			// fromSelectBtn
+			// 
+			this.fromSelectBtn.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+			this.fromSelectBtn.Location = new System.Drawing.Point(444, 24);
+			this.fromSelectBtn.Name = "fromSelectBtn";
+			this.fromSelectBtn.Size = new System.Drawing.Size(75, 23);
+			this.fromSelectBtn.TabIndex = 4;
+			this.fromSelectBtn.Text = "Browse";
+			this.fromSelectBtn.UseVisualStyleBackColor = true;
+			this.fromSelectBtn.Click += new System.EventHandler(this.fromSelectButton_Click);
 			// 
 			// SecureMoveErasureTargetConfigurer
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+			this.Controls.Add(this.fromSelectBtn);
+			this.Controls.Add(this.moveFolderRadio);
+			this.Controls.Add(this.moveFileRadio);
 			this.Controls.Add(this.fromTxt);
 			this.Controls.Add(this.fromLbl);
 			this.Controls.Add(this.toTxt);
-			this.Controls.Add(this.fromSelectButton);
 			this.Controls.Add(this.toLbl);
 			this.Controls.Add(this.toSelectButton);
 			this.Font = new System.Drawing.Font("Segoe UI", 9F);
 			this.Name = "SecureMoveErasureTargetConfigurer";
-			this.Size = new System.Drawing.Size(519, 55);
-			this.fromBrowseMenu.ResumeLayout(false);
+			this.Size = new System.Drawing.Size(519, 82);
 			this.ResumeLayout(false);
 			this.PerformLayout();
 
@@ -167,16 +165,16 @@ namespace Eraser.DefaultPlugins
 
 		#endregion
 
-		private System.Windows.Forms.OpenFileDialog fileDialog;
+		private System.Windows.Forms.OpenFileDialog openFileDialog;
 		private System.Windows.Forms.FolderBrowserDialog folderDialog;
 		private System.Windows.Forms.Label fromLbl;
 		private System.Windows.Forms.TextBox fromTxt;
 		private System.Windows.Forms.Label toLbl;
 		private System.Windows.Forms.TextBox toTxt;
-		private System.Windows.Forms.SplitButton fromSelectButton;
-		private System.Windows.Forms.ContextMenuStrip fromBrowseMenu;
-		private System.Windows.Forms.ToolStripMenuItem fromSelectFile;
-		private System.Windows.Forms.ToolStripMenuItem fromSelectFolder;
 		private System.Windows.Forms.Button toSelectButton;
+		private System.Windows.Forms.RadioButton moveFileRadio;
+		private System.Windows.Forms.RadioButton moveFolderRadio;
+		private System.Windows.Forms.Button fromSelectBtn;
+		private System.Windows.Forms.SaveFileDialog saveFileDialog;
 	}
 }
