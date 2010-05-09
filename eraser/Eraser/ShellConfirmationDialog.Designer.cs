b8d9b78797c8f1d0785f88b24fb41677d8fe9a95
aramacciotti <aramacciotti@5a19fcc5-093c-0410-9d93-cd3e460611b7>
2010-05-09 10:17:37 +0000

    Italian translation

--- eraser/Eraser/ShellConfirmationDialog.Designer.cs
+++ eraser/Eraser/ShellConfirmationDialog.Designer.cs
@@ -28,66 +28,89 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
-			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ShellConfirmationDialog));
-			this.Image = new System.Windows.Forms.PictureBox();
-			this.Message = new System.Windows.Forms.Label();
-			this.YesBtn = new System.Windows.Forms.Button();
-			this.NoBtn = new System.Windows.Forms.Button();
-			this.OptionsButton = new System.Windows.Forms.Button();
-			((System.ComponentModel.ISupportInitialize)(this.Image)).BeginInit();
-			this.SuspendLayout();
-			// 
-			// Image
-			// 
-			resources.ApplyResources(this.Image, "Image");
-			this.Image.Name = "Image";
-			this.Image.TabStop = false;
-			// 
-			// Message
-			// 
-			resources.ApplyResources(this.Message, "Message");
-			this.Message.Name = "Message";
-			// 
-			// YesBtn
-			// 
-			resources.ApplyResources(this.YesBtn, "YesBtn");
-			this.YesBtn.DialogResult = System.Windows.Forms.DialogResult.Yes;
-			this.YesBtn.Name = "YesBtn";
-			this.YesBtn.UseVisualStyleBackColor = true;
-			// 
-			// NoBtn
-			// 
-			resources.ApplyResources(this.NoBtn, "NoBtn");
-			this.NoBtn.DialogResult = System.Windows.Forms.DialogResult.No;
-			this.NoBtn.Name = "NoBtn";
-			this.NoBtn.UseVisualStyleBackColor = true;
-			// 
-			// OptionsButton
-			// 
-			resources.ApplyResources(this.OptionsButton, "OptionsButton");
-			this.OptionsButton.Name = "OptionsButton";
-			this.OptionsButton.UseVisualStyleBackColor = true;
-			this.OptionsButton.Click += new System.EventHandler(this.OptionsButton_Click);
-			// 
-			// ShellConfirmationDialog
-			// 
-			this.AcceptButton = this.NoBtn;
-			resources.ApplyResources(this, "$this");
-			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.CancelButton = this.YesBtn;
-			this.Controls.Add(this.OptionsButton);
-			this.Controls.Add(this.NoBtn);
-			this.Controls.Add(this.YesBtn);
-			this.Controls.Add(this.Message);
-			this.Controls.Add(this.Image);
-			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
-			this.MaximizeBox = false;
-			this.MinimizeBox = false;
-			this.Name = "ShellConfirmationDialog";
-			this.ShowInTaskbar = false;
-			((System.ComponentModel.ISupportInitialize)(this.Image)).EndInit();
-			this.ResumeLayout(false);
-			this.PerformLayout();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ShellConfirmationDialog));
+            this.Image = new System.Windows.Forms.PictureBox();
+            this.Message = new System.Windows.Forms.Label();
+            this.YesBtn = new System.Windows.Forms.Button();
+            this.NoBtn = new System.Windows.Forms.Button();
+            this.OptionsButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.Image)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Image
+            // 
+            this.Image.AccessibleDescription = null;
+            this.Image.AccessibleName = null;
+            resources.ApplyResources(this.Image, "Image");
+            this.Image.BackgroundImage = null;
+            this.Image.Font = null;
+            this.Image.ImageLocation = null;
+            this.Image.Name = "Image";
+            this.Image.TabStop = false;
+            // 
+            // Message
+            // 
+            this.Message.AccessibleDescription = null;
+            this.Message.AccessibleName = null;
+            resources.ApplyResources(this.Message, "Message");
+            this.Message.Name = "Message";
+            // 
+            // YesBtn
+            // 
+            this.YesBtn.AccessibleDescription = null;
+            this.YesBtn.AccessibleName = null;
+            resources.ApplyResources(this.YesBtn, "YesBtn");
+            this.YesBtn.BackgroundImage = null;
+            this.YesBtn.DialogResult = System.Windows.Forms.DialogResult.Yes;
+            this.YesBtn.Font = null;
+            this.YesBtn.Name = "YesBtn";
+            this.YesBtn.UseVisualStyleBackColor = true;
+            // 
+            // NoBtn
+            // 
+            this.NoBtn.AccessibleDescription = null;
+            this.NoBtn.AccessibleName = null;
+            resources.ApplyResources(this.NoBtn, "NoBtn");
+            this.NoBtn.BackgroundImage = null;
+            this.NoBtn.DialogResult = System.Windows.Forms.DialogResult.No;
+            this.NoBtn.Font = null;
+            this.NoBtn.Name = "NoBtn";
+            this.NoBtn.UseVisualStyleBackColor = true;
+            // 
+            // OptionsButton
+            // 
+            this.OptionsButton.AccessibleDescription = null;
+            this.OptionsButton.AccessibleName = null;
+            resources.ApplyResources(this.OptionsButton, "OptionsButton");
+            this.OptionsButton.BackgroundImage = null;
+            this.OptionsButton.Font = null;
+            this.OptionsButton.Name = "OptionsButton";
+            this.OptionsButton.UseVisualStyleBackColor = true;
+            this.OptionsButton.Click += new System.EventHandler(this.OptionsButton_Click);
+            // 
+            // ShellConfirmationDialog
+            // 
+            this.AcceptButton = this.NoBtn;
+            this.AccessibleDescription = null;
+            this.AccessibleName = null;
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = null;
+            this.CancelButton = this.YesBtn;
+            this.Controls.Add(this.OptionsButton);
+            this.Controls.Add(this.NoBtn);
+            this.Controls.Add(this.YesBtn);
+            this.Controls.Add(this.Message);
+            this.Controls.Add(this.Image);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = null;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "ShellConfirmationDialog";
+            this.ShowInTaskbar = false;
+            ((System.ComponentModel.ISupportInitialize)(this.Image)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
 
 		}
 
