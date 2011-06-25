8c371295bed2742464c0c1b36dd428a43e85815e
lowjoel <lowjoel@5a19fcc5-093c-0410-9d93-cd3e460611b7>
2011-06-25 14:22:15 +0000

    Apache would send responses in chunks if we don't calculate our own response length. This causes 6.0.8 and earlier to choke, so buffer our output and tell the client how long our response is.

--- trunk/website/scripts/updates/index.php
+++ trunk/website/scripts/updates/index.php
@@ -7,7 +7,10 @@ $versionMatch = array();
 if (empty($action) || empty($version) || !preg_match('/([0-9]+).([0-9]+).([0-9]+).([0-9]+)/', $version, $versionMatch))
 	exit;
 
+ob_start();
 header('content-type: application/xml');
 $downloadsList = intval($versionMatch[2]) == 0 ? new UpdateList1() : new UpdateList1_1();
 $downloadsList->Output($version);
+
+header('content-length: ' . ob_get_length());
 ?>
