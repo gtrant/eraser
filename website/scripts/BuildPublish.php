5bcadaf6171338dd0999eb315ba0d80230f3e2d7
lowjoel <lowjoel@5a19fcc5-093c-0410-9d93-cd3e460611b7>
2011-11-26 02:01:07 +0000

    Factor the utility functions used by the Build slaves to a separate BuildUtil file which is referenced by both the build server and build client scripts.

--- website/scripts/BuildPublish.php
+++ website/scripts/BuildPublish.php
@@ -17,56 +17,6 @@ require_once('Credentials.php');
 require_once('Database.php');
 require_once('Build.php');
 
-/**
- * Uploads the contents of a stream to a particular URL.
- *
- * @param string $url      The URL of the destination of the upload.
- * @param string $stream   The source stream for upload.
- * @param string $username The username of the user, if required.
- * @param string $password The password of the user, if required.
- */
-function Upload($url, $stream, $username = '', $password = '')
-{
-	printf('Uploading file to %s... ', $url);
-
-	$curl = curl_init($url);
-	curl_setopt($curl, CURLOPT_USERPWD, sprintf('%s:%s', $username, $password));
-	curl_setopt($curl, CURLOPT_UPLOAD, true);
-	curl_setopt($curl, CURLOPT_INFILE, $stream);
-
-	if (curl_exec($curl) === false)
-		throw new Exception('cURL Error: ' . curl_error($curl));
-	curl_close($curl);
-
-	printf("File uploaded.\n");
-}
-
-function Delete($url, $username = '', $password = '')
-{
-	printf('Deleting %s... ', $url);
-
-	$curl = curl_init($url);
-	curl_setopt($curl, CURLOPT_USERPWD, sprintf('%s:%s', $username, $password));
-
-	//Create a temp stream to upload to the old path (to zero the length)
-	$stream = fopen('php://temp', 'r');
-	curl_setopt($curl, CURLOPT_UPLOAD, true);
-	curl_setopt($curl, CURLOPT_INFILE, $stream);
-
-	//Parse the URL to get the path to delete
-	$path = parse_url($url, PHP_URL_PATH);
-	curl_setopt($curl, CURLOPT_POSTQUOTE, array(
-		sprintf('rm "%s"', $path)
-	));
-
-	if (curl_exec($curl) === false)
-		throw new Exception('cURL Error: ' . curl_error($curl));
-	fclose($stream);
-	curl_close($curl);
-
-	printf("File deleted.\n");
-}
-
 $file = fopen($argv[3], 'rb');
 if (!$file)
 {
