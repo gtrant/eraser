7bdff2f52e3c39314b7c09b6bae2743f356272ac
lowjoel <lowjoel@5a19fcc5-093c-0410-9d93-cd3e460611b7>
2012-05-22 03:48:22 +0000

    Implement querying SourceForge for the number of downloads using the API defined in http://sourceforge.net/p/forge/documentation/Download%20Stats%20API/

--- website/scripts/SourceForge.php
+++ website/scripts/SourceForge.php
@@ -38,46 +38,18 @@ class SourceForge
 		//Get the path.
 		$pathComponents = explode('/', $urlInfo['path']);
 
-		//The first three components is the page we need to download.
-		$downloadPage = implode('/', array_slice($pathComponents, 0, 4));
+		//Retain all path components except the final /download
+		$downloadPage = implode('/', array_merge(array_slice($pathComponents, 0, -1), array('stats', 'json')));
+		$downloadPage .= '?start_date=2001-10-04&end_date=' . date('Y-m-d');
 
-		//Parse the download page.
-		$document = new DOMDocument();
-
-		//Get the download page, using the cache if it exists.
-		$document->loadHTML(SourceForge::Download('http://sourceforge.net' . $downloadPage, 60 * 60));
-		foreach ($document->getElementsByTagName('a') as $element)
+		//Get the JSON and parse it
+		$document = json_decode(SourceForge::Download('http://sourceforge.net' . $downloadPage, 60 * 60));
+		if (!empty($document->total))
 		{
-			if (!is_a($element, 'DOMElement'))
-				continue;
-
-			//$parent is the <td> node.
-			$parent = $element->parentNode;
-			if (is_a($parent, 'DOMElement') && $parent->tagName == 'td' &&
-				$parent->getAttribute('class') == 'tree')
-			{
-				$downloadUrlInfo = parse_url(urldecode($element->getAttribute('href')));
-				if ($urlInfo['path'] == $downloadUrlInfo['path'])
-				{
-					//$grandParent is the <tr> node.
-					$grandParent = $parent->parentNode;
-
-					//Find the download column (currently the 5th)
-					$i = 0;
-					foreach ($grandParent->getElementsByTagName('td') as $cell)
-					{
-						if (++$i == 5)
-						{
-							//We are at the 5th column, return the contents as an integer.
-							$result = str_replace(',', '', $cell->textContent);
-							return intval($result);
-						}
-					}
-				}
-			}
+			return intval($document->total);
 		}
 
-		throw new Exception('Could not find the node containing the download.');
+		throw new Exception('Could not find the node containing the number of downloads.');
 	}
 
 	/**
