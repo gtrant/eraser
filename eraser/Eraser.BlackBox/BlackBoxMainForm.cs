b315e2a19243091c22a43522ff478c92bdf565ac
lowjoel <lowjoel@5a19fcc5-093c-0410-9d93-cd3e460611b7>
2012-05-24 09:31:52 +0000

    Add a menu strip to the Reports List View containing a Delete command like the Windows Problems and Solutions dialog.

--- eraser/Eraser.BlackBox/BlackBoxMainForm.cs
+++ eraser/Eraser.BlackBox/BlackBoxMainForm.cs
@@ -63,6 +63,18 @@ namespace Eraser.BlackBox
 			Process.Start((ReportsLv.SelectedItems[0].Tag as BlackBoxReport).Path);
 		}
 
+		private void ReportsMenuStrip_Opening(object sender, CancelEventArgs e)
+		{
+			if (ReportsLv.SelectedItems.Count == 0)
+			{
+				e.Cancel = true;
+			}
+		}
+
+		private void deleteToolStripMenuItem_Click(object sender, EventArgs e)
+		{
+		}
+
 		private void DataCollectionPolicyLbl_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
 		{
 			Process.Start("http://eraser.heidi.ie/trac/wiki/DataCollectionPolicy");
