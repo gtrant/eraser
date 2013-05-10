f9efcfa8d942c28875e2bb38207946d06de81a7b
lowjoel <lowjoel@5a19fcc5-093c-0410-9d93-cd3e460611b7>
2013-05-10 05:20:03 +0000

    Also check the 64-bit Program Files directory, in the event we are compiling using a 32-bit shell.

--- trunk/eraser/UpdateVersions.bat
+++ trunk/eraser/UpdateVersions.bat
@@ -1,8 +1,17 @@
 @echo off
 rem Replace all template version placeholders in the given template file for keywords.
 
-if not exist "%ProgramFiles%\TortoiseSVN\bin\SubWCRev.exe" goto searchWoWProgramFiles
+if not exist "%ProgramFiles%\TortoiseSVN\bin\SubWCRev.exe" goto searchNativeProgramFiles
 "%ProgramFiles%\TortoiseSVN\bin\SubWCRev.exe" %1 %2 %3 -f
+if ERRORLEVEL 1 (
+	exit /b %ERRORLEVEL%
+)
+goto end
+
+:searchNativeProgramFiles
+
+if not exist "%ProgramW6432%\TortoiseSVN\bin\SubWCRev.exe" goto searchWoWProgramFiles
+"%ProgramW6432%\TortoiseSVN\bin\SubWCRev.exe" %1 %2 %3 -f
 if ERRORLEVEL 1 (
 	exit /b %ERRORLEVEL%
 )
