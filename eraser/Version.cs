0e5774799a3fc56437cce5503755ce9858051c51
gtrant <gtrant@users.noreply.github.com>
2014-10-02 12:47:16 +0000

    Missing file

---
+++ eraser/Version.cs
@@ -0,0 +1,46 @@
+/* 
+ * $Id: Version.cs.in 2922 2013-05-10 05:19:13Z lowjoel $
+ * Copyright 2008-2013 The Eraser Project
+ * Original Author: Joel Low <lowjoel@users.sourceforge.net>
+ * Modified By: 
+ * 
+ * This file is part of Eraser.
+ * 
+ * Eraser is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later
+ * version.
+ * 
+ * Eraser is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ * 
+ * A copy of the GNU General Public License can be found at
+ * <http://www.gnu.org/licenses/>.
+ */
+using System;
+using System.Reflection;
+using System.Diagnostics;
+
+[assembly: AssemblyFileVersion("6.1.0.2948")]
+[assembly: AssemblyVersion("6.1.0.2671")]
+
+namespace Eraser {
+	internal static class BuildInfo
+	{
+		public static readonly DateTime BuildDate = DateTime.Parse("2014/04/30 14:10:22",
+			System.Globalization.CultureInfo.InvariantCulture);
+		public const bool CustomBuild = true;
+		public static Version AssemblyFileVersion
+		{
+			get
+			{
+				FileVersionInfo version = FileVersionInfo.GetVersionInfo(
+					Assembly.GetExecutingAssembly().Location);
+
+				return new Version(version.FileMajorPart, version.FileMinorPart,
+					version.FileBuildPart, version.FilePrivatePart);
+			}
+		}
+	}
+}
