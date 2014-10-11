99bc5f2681e0ce85eca7b0d2f2e8dd2620dcbfa8
gtrant <gtrant@5a19fcc5-093c-0410-9d93-cd3e460611b7>
2014-10-11 13:19:35 +0000

    Update version number, tidy up plugin code

--- trunk/eraser/Eraser.Manager/DirectExecutor.cs
+++ trunk/eraser/Eraser.Manager/DirectExecutor.cs
@@ -3,7 +3,7 @@
  * Copyright 2008-2013 The Eraser Project
  * Original Author: Joel Low <lowjoel@users.sourceforge.net>
  * Modified By: Kasra Nassiri <cjax@users.sourceforge.net> @17/10/2008
- * Modified By: 
+ * Modified By: Garrett Trant <gtrant@users.sourceforge.net>
  * 
  * This file is part of Eraser.
  * 
@@ -56,26 +56,28 @@ namespace Eraser.Manager
 				return;
 
 			if (disposing)
-			{
-				thread.Abort();
-				schedulerInterrupt.Set();
-
-				//Wait for the executor thread to exit -- we call some event functions
-				//and these events may need invocation on the main thread. So,
-				//pump messages from the main thread until the thread exits.
-				if (System.Windows.Forms.Application.MessageLoop)
-				{
-					if (!thread.Join(new TimeSpan(0, 0, 0, 0, 100)))
-						System.Windows.Forms.Application.DoEvents();
-				}
-
-				//If we are disposing on a secondary thread, or a thread without
-				//a message loop, just wait for the thread to exit indefinitely
-				else
-					thread.Join();
-
-				schedulerInterrupt.Close();
-			}
+{
+    thread.Abort();
+    schedulerInterrupt.Set();
+    //Wait for the executor thread to exit -- we call some event functions
+    //and these events may need invocation on the main thread. So,
+    //pump messages from the main thread until the thread exits.
+    if (System.Windows.Forms.Application.MessageLoop)
+    {
+        if (!thread.Join(new TimeSpan(0, 0, 0, 0, 100)))
+            System.Windows.Forms.Application.DoEvents();
+    }
+    //If we are disposing on a secondary thread, or a thread without
+    //a message loop, just wait for the thread to exit indefinitely
+    else
+        thread.Join();
+    schedulerInterrupt.Close();
+    if (schedulerInterrupt != null)
+    {
+        schedulerInterrupt.Dispose();
+        schedulerInterrupt = null;
+    }
+}
 
 			thread = null;
 			schedulerInterrupt = null;
@@ -349,6 +351,16 @@ namespace Eraser.Manager
 				: base(executor)
 			{
 			}
+            /// <summary>
+            /// Constructor.
+            /// </summary>
+            /// <param name="executor">The <seealso cref="Executor"/> object owning
+            /// this task list.</param>
+            protected DirectExecutorTasksCollection(Executor executor)
+                : base(executor)
+            {
+                
+            }
 
 			#region IList<Task> Members
 			public override int IndexOf(Task item)
@@ -476,8 +488,7 @@ namespace Eraser.Manager
 
 			public override void SaveToFile(string file)
 			{
-				XmlWriterSettings settings = new XmlWriterSettings();
-				settings.Indent = true;
+                XmlWriterSettings settings = new XmlWriterSettings() { Indent = true };
 				lock (list)
 				using (XmlWriter writer = XmlWriter.Create(file, settings))
 				{
